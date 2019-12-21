0f56bcabc88852873e9d46f28b86f1dec5a82216
TinyKitten <catwalk8205@gmail.com>
2019-12-21 22:36:56 +0900

    位置情報精度変更

--- src/store/actions/locationAsync.ts
+++ src/store/actions/locationAsync.ts
@@ -22,7 +22,8 @@ export const updateLocationAsync = (): ThunkAction<void, AppState, null, Action<
   try {
     await askPermission();
     Location.watchPositionAsync({
-      accuracy: Location.Accuracy.BestForNavigation,
+      enableHighAccuracy: true,
+      accuracy: Location.Accuracy.Highest,
     }, (data) => {
       dispatch(updateLocationSuccess(data));
       if (data.coords.accuracy > BAD_ACCURACY_THRESHOLD) {
