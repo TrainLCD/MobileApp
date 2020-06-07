1b4dbd457c4bc73aa7af1a2911427c8f9fcd60f3
TinyKitten <ts@tinykitten.me>
2020-06-08 01:48:42 +0900

    JR５線以上私鉄４線以上でJR線で省略

--- src/constants.ts
+++ src/constants.ts
@@ -31,5 +31,6 @@ export const getArrivedThreshold = (lineType: LineType): number => {
   }
 };
 
-export const OMIT_JR_THRESHOLD = 3; // これ以上JR線があったら「JR線」で省略しよう
+export const OMIT_JR_THRESHOLD = 5; // これ以上JR線があったら「JR線」で省略しよう
 export const JR_LINE_MAX_ID = 6;
+export const MAX_PRIVATE_COUNT_FOR_OMIT_JR = 4; // これ以上私鉄があったらJR側を「JR線」で省略
