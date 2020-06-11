b368ce75324793e724006bf839e70e823da8a609
TinyKitten <ts@tinykitten.me>
2020-06-11 18:17:21 +0900

    山手線を選ぶとクラッシュするバグを修正

--- src/screens/SelectBound/index.tsx
+++ src/screens/SelectBound/index.tsx
@@ -132,9 +132,11 @@ const SelectBoundScreen: React.FC<Props> = ({
   let computedOutboundStation: Station;
   if (yamanoteLine) {
     if (inbound) {
-      computedInboundStation = inbound.station;
-      computedOutboundStation = outboundStation;
-    } else {
+      if (inbound) {
+        computedInboundStation = inbound.station;
+        computedOutboundStation = outboundStation;
+      }
+    } else if (outbound) {
       computedInboundStation = inboundStation;
       computedOutboundStation = outbound.station;
     }
