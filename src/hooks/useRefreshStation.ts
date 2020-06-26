037ea503791fce17c4847b7e073b9bd314301e85
TinyKitten <ts@tinykitten.me>
2020-06-26 11:47:23 +0900

    useRefreshStation の一部関数を外側に移動

--- src/hooks/useRefreshStation.ts
+++ src/hooks/useRefreshStation.ts
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { useCallback, Dispatch, useEffect } from 'react';
+import { Dispatch, useEffect } from 'react';
 import { getArrivedThreshold, getApproachingThreshold } from '../constants';
 import { Station, Line } from '../models/StationAPI';
 import { TrainLCDAppState } from '../store';
@@ -12,6 +12,34 @@ import {
   refreshNearestStation,
 } from '../store/actions/station';
 
+const isArrived = (nearestStation: Station, currentLine: Line): boolean => {
+  if (!nearestStation) {
+    return false;
+  }
+  const ARRIVED_THRESHOLD = getArrivedThreshold(currentLine?.lineType);
+  return nearestStation.distance < ARRIVED_THRESHOLD;
+};
+
+const isApproaching = (
+  nextStation: Station,
+  nearestStation: Station,
+  currentLine: Line
+): boolean => {
+  if (!nextStation) {
+    return false;
+  }
+  const APPROACHING_THRESHOLD = getApproachingThreshold(currentLine?.lineType);
+  // APPROACHING_THRESHOLD以上次の駅から離れている: つぎは
+  // APPROACHING_THRESHOLDより近い: まもなく
+  return (
+    nearestStation.distance < APPROACHING_THRESHOLD &&
+    nearestStation.groupId === nextStation.groupId
+  );
+};
+
+const getRefreshConditions = (station: Station, currentLine: Line): boolean =>
+  station.distance < getArrivedThreshold(currentLine?.lineType);
+
 const useRefreshStation = (): void => {
   const dispatch = useDispatch<Dispatch<StationActionTypes>>();
   const { stations } = useSelector((state: TrainLCDAppState) => state.station);
@@ -23,56 +51,14 @@ const useRefreshStation = (): void => {
     (state: TrainLCDAppState) => state.location.location
   );
 
-  const isArrived = useCallback(
-    (nearestStation: Station, currentLine: Line): boolean => {
-      if (!nearestStation) {
-        return false;
-      }
-      const ARRIVED_THRESHOLD = getArrivedThreshold(currentLine?.lineType);
-      return nearestStation.distance < ARRIVED_THRESHOLD;
-    },
-    []
-  );
-
-  const isApproaching = useCallback(
-    (
-      nextStation: Station,
-      nearestStation: Station,
-      currentLine: Line
-    ): boolean => {
-      if (!nextStation) {
-        return false;
-      }
-      const APPROACHING_THRESHOLD = getApproachingThreshold(
-        currentLine?.lineType
-      );
-      // APPROACHING_THRESHOLD以上次の駅から離れている: つぎは
-      // APPROACHING_THRESHOLDより近い: まもなく
-      return (
-        nearestStation.distance < APPROACHING_THRESHOLD &&
-        nearestStation.groupId === nextStation.groupId
-      );
-    },
-    []
-  );
-
-  const getRefreshConditions = useCallback(
-    (station: Station, currentLine: Line): boolean =>
-      station.distance < getArrivedThreshold(currentLine?.lineType),
-    []
-  );
-
   const { latitude, longitude } = coords;
 
   useEffect(() => {
     const scoredStations = calcStationDistances(stations, latitude, longitude);
     const nearestStation = scoredStations[0];
+    const nextStaton = leftStations[1];
     const arrived = isArrived(nearestStation, selectedLine);
-    const approaching = isApproaching(
-      leftStations[1],
-      nearestStation,
-      selectedLine
-    );
+    const approaching = isApproaching(nextStaton, nearestStation, selectedLine);
     const conditionPassed = getRefreshConditions(nearestStation, selectedLine);
     dispatch(updateScoredStations(scoredStations));
     dispatch(updateArrived(arrived));
@@ -81,15 +67,7 @@ const useRefreshStation = (): void => {
       dispatch(refreshNearestStation(nearestStation));
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    dispatch,
-    getRefreshConditions,
-    isApproaching,
-    isArrived,
-    latitude,
-    longitude,
-    selectedLine,
-  ]);
+  }, [dispatch, latitude, longitude, selectedLine]);
 };
 
 export default useRefreshStation;
