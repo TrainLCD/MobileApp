8970b930a6327b46a29bf6448da28df9fb332711
TinyKitten <ts@tinykitten.me>
2020-10-06 11:03:38 +0900

    android版link処理

--- android/app/src/main/java/me/tinykitten/trainlcd/generated/BasePackageList.java
+++ android/app/src/main/java/me/tinykitten/trainlcd/generated/BasePackageList.java
@@ -24,7 +24,8 @@ public class BasePackageList {
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
         new expo.modules.taskManager.TaskManagerPackage(),
-        new expo.modules.updates.UpdatesPackage()
+        new expo.modules.updates.UpdatesPackage(),
+        new expo.modules.webbrowser.WebBrowserPackage()
     );
   }
 }
