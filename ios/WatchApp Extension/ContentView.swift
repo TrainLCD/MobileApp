57d60a060a87bb6f5e8ca603fe0095f60ff90494
TinyKitten <ts@tinykitten.me>
2020-12-19 16:43:34 +0900

    watchapp refactor

--- ios/WatchApp Extension/ContentView.swift
+++ ios/WatchApp Extension/ContentView.swift
@@ -9,32 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var connector = PhoneConnector()
+  @ObservedObject var connector = PhoneConnector()
   
-    var body: some View {
-      VStack{
-        Text("\(connector.receivedState)")
-              .multilineTextAlignment(.center)
+  var body: some View {
+    VStack{
+      Text("\(connector.receivedState)")
+            .multilineTextAlignment(.center)
           .font(.subheadline)
-        Text("\(connector.receivedStationName)")
-              .multilineTextAlignment(.center)
-          .font(.title2)
-        List {
-          ForEach(connector.lines.indices, id: \.self) { index in
-            Text(connector.lines[index])
+      Text("\(connector.receivedStationName)")
+        .multilineTextAlignment(.center)
+        .font(.title2)
+      List {
+        ForEach(connector.lines.indices, id: \.self) { index in
+          Text(connector.lines[index])
               .listRowBackground(Color.init(hex: connector.linesColor[index]))
-            
-          }
         }
       }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-      Group {
-        ContentView()
-        ContentView()
-      }
+      ContentView()
     }
 }
