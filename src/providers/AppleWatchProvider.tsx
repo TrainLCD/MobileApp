10c775597b5c0f42c4c9f7c1bef70dcdd7157654
TinyKitten <ts@tinykitten.me>
2020-12-24 15:04:05 +0900

    Apple Watchを見ているときに継続的に駅情報を送信するようにした

--- src/providers/AppleWatchProvider.tsx
+++ src/providers/AppleWatchProvider.tsx
@@ -1,13 +1,10 @@
-import React, { useCallback, useEffect, useMemo } from 'react';
-import { Platform, PlatformIOSStatic } from 'react-native';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { sendMessage, watchEvents } from 'react-native-watch-connectivity';
 import { useRecoilValue } from 'recoil';
 import lineState from '../store/atoms/line';
 import navigationState from '../store/atoms/navigation';
 import stationState from '../store/atoms/station';
 import { isLoopLine } from '../utils/loopLine';
-
-const { isPad } = Platform as PlatformIOSStatic;
 
 type Props = {
   children: React.ReactNode;
@@ -17,6 +14,7 @@ const AppleWatchProvider: React.FC<Props> = ({ children }: Props) => {
   const { station, stations, selectedDirection } = useRecoilValue(stationState);
   const { headerState, leftStations } = useRecoilValue(navigationState);
   const { selectedLine } = useRecoilValue(lineState);
+  const [wcReachable, setWCReachable] = useState(false);
 
   const nextStation = leftStations[1];
 
@@ -51,19 +49,17 @@ const AppleWatchProvider: React.FC<Props> = ({ children }: Props) => {
         state: headerState,
         station: switchedStation,
       });
-      if (selectedLine) {
-        sendMessage({
-          stationList:
-            selectedDirection === 'INBOUND'
-              ? inboundStations
-              : outboundStations,
-          selectedLine,
-        });
-      } else {
-        sendMessage({
-          stationList: [],
-        });
-      }
+    }
+    if (selectedLine) {
+      sendMessage({
+        stationList:
+          selectedDirection === 'INBOUND' ? inboundStations : outboundStations,
+        selectedLine,
+      });
+    } else {
+      sendMessage({
+        stationList: [],
+      });
     }
   }, [
     headerState,
@@ -76,25 +72,23 @@ const AppleWatchProvider: React.FC<Props> = ({ children }: Props) => {
   ]);
 
   useEffect(() => {
-    if (Platform.OS === 'android' || isPad) {
-      return (): void => {
-        // nothing to do.
-      };
-    }
-
     const unsubscribeReachabilitySub = watchEvents.addListener(
       'reachability',
       (reachable: boolean) => {
-        if (reachable) {
-          sendToWatch();
-        }
+        setWCReachable(reachable);
       }
     );
 
     return (): void => {
       unsubscribeReachabilitySub();
     };
-  }, [sendToWatch]);
+  }, []);
+
+  useEffect(() => {
+    if (wcReachable) {
+      sendToWatch();
+    }
+  }, [sendToWatch, wcReachable]);
 
   return <>{children}</>;
 };
