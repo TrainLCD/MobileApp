d959182cef073cb0fce1bddbfa5eaa743adeeaf9
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-01-19 18:52:53 +0900

    expo 40

--- android/app/src/main/java/me/tinykitten/trainlcd/generated/BasePackageList.java
+++ android/app/src/main/java/me/tinykitten/trainlcd/generated/BasePackageList.java
@@ -20,6 +20,7 @@ public class BasePackageList {
         new expo.modules.location.LocationPackage(),
         new expo.modules.notifications.NotificationsPackage(),
         new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.securestore.SecureStorePackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
         new expo.modules.taskManager.TaskManagerPackage(),
