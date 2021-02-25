43f7d23c0a3d690bfb4ad0e98f39e98eedd1978e
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-02-25 22:56:28 +0900

    dotenv廃止

--- src/@types/react-native-dotenv.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '@env' {
-  // eslint-disable-next-line import/prefer-default-export
-  export const API_URL: string;
-}
--- src/api/apollo.ts
+++ src/api/apollo.ts
@@ -1,6 +1,4 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-// eslint-disable-next-line import/no-unresolved
-import { API_URL } from '@env';
 
 const client = new ApolloClient({
   uri: 'https://sapi.tinykitten.me/graphql',
