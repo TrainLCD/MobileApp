8fb7676888dc3e751a1f003bb2fda29af80988ab
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-03-08 03:14:45 +0900

    私鉄特急対応

--- src/components/HeaderJRWest/index.tsx
+++ src/components/HeaderJRWest/index.tsx
@@ -448,7 +448,12 @@ const HeaderJRWest: React.FC<CommonHeaderProps> = ({
       default:
         break;
     }
-    if (trainType?.id >= 200 && trainType?.id <= 300) {
+    if (
+      // 200~300 JR特急
+      // 500~600 私鉄特急
+      (trainType?.id >= 200 && trainType?.id <= 300) ||
+      (trainType?.id >= 500 && trainType?.id <= 600)
+    ) {
       return fetchJRWLtdExpressLogo();
     }
     if (trainTypeName.includes('特快')) {
@@ -474,6 +479,7 @@ const HeaderJRWest: React.FC<CommonHeaderProps> = ({
     }
     return fetchJRWLocalLogo();
   }, [
+    fetchJREChuoLineSpecialRapidLogo,
     fetchJRECommuterRapidLogo,
     fetchJRECommuterSpecialRapidLogo,
     fetchJRESpecialRapidLogo,
