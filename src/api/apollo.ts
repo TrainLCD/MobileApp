9d5dd92416fdd0dba3a0f09db7c98cfbf7b0b683
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-04-03 17:52:57 +0900

    dotenv対応

--- src/api/apollo.ts
+++ src/api/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://sapi.tinykitten.me/graphql',
+  uri: __DEV__ ? process.env.API_URL_STG : process.env.API_URL_PROD,
   cache: new InMemoryCache(),
 });
 
