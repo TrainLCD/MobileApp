aa9248a9ba33b95ba92833a26327611974f5e077
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-05-06 21:02:14 +0900

    bump version

--- android/app/src/main/java/me/tinykitten/trainlcd/generated/BasePackageList.java
+++ android/app/src/main/java/me/tinykitten/trainlcd/generated/BasePackageList.java
@@ -8,6 +8,7 @@ public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
         new expo.modules.application.ApplicationPackage(),
+        new expo.modules.av.AVPackage(),
         new expo.modules.backgroundfetch.BackgroundFetchPackage(),
         new expo.modules.constants.ConstantsPackage(),
         new expo.modules.errorrecovery.ErrorRecoveryPackage(),
