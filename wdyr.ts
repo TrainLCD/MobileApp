931b203d7ba1f127319e3c67ac64f1e648202668
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-08-27 13:35:10 +0900

    wdyr有効化

---
+++ wdyr.ts
@@ -0,0 +1,12 @@
+/// <reference types="@welldone-software/why-did-you-render" />
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import React from 'react';
+
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
