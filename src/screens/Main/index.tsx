195b763e08a74a81c21f1420359fbffb35df43ea
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2021-08-29 18:35:29 +0900

    位置情報間引き戻した

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -178,6 +178,8 @@ const MainScreen: React.FC = () => {
     Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
       accuracy: Location.Accuracy.High,
       activityType: Location.ActivityType.Other,
+      timeInterval: 1000,
+      distanceInterval: 100,
       foregroundService: {
         notificationTitle: translate('bgAlertTitle'),
         notificationBody: translate('bgAlertContent'),
