35d90c6c352176e7a56eb08f7ff949c987a7bcff
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-05-16 19:43:16 +0900

    埼京線始発表示削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useRecoilValue } from 'recoil';
 import AppTheme from '../models/Theme';
-import mirroringShareState from '../store/atoms/mirroringShare';
-import navigationState from '../store/atoms/navigation';
-import stationState from '../store/atoms/station';
 import themeState from '../store/atoms/theme';
 import CommonHeaderProps from './CommonHeaderProps';
 import HeaderSaikyo from './HeaderSaikyo';
-import HeaderSaikyoDepature from './HeaderSaikyoDepature';
 import HeaderTokyoMetro from './HeaderTokyoMetro';
 import HeaderTY from './HeaderTY';
 
@@ -18,26 +14,6 @@ const Header = ({
   isLast,
 }: CommonHeaderProps): React.ReactElement => {
   const { theme } = useRecoilValue(themeState);
-  const { headerState } = useRecoilValue(navigationState);
-  const { selectedBound } = useRecoilValue(stationState);
-  const { subscribing } = useRecoilValue(mirroringShareState);
-  const [isDepartured, setIsDepartured] = useState(false);
-
-  // 埼京線始発テーマ用
-  useEffect(() => {
-    // ミラーリングシェアに接続しているときになにかの駅間だったときに
-    // HeaderSaikyoDepatureが出続けて不便なのでシェアを購読中は出さない
-    if (!headerState.startsWith('CURRENT') || subscribing) {
-      setIsDepartured(true);
-    }
-  }, [headerState, subscribing]);
-
-  // メイン画面から戻ったときに出発済みステートを初期化する
-  useEffect(() => {
-    if (!selectedBound) {
-      setIsDepartured(false);
-    }
-  }, [selectedBound]);
 
   switch (theme) {
     case AppTheme.TokyoMetro:
@@ -59,16 +35,6 @@ const Header = ({
         />
       );
     case AppTheme.Saikyo:
-      if (headerState.startsWith('CURRENT') && !isDepartured && selectedBound) {
-        return (
-          <HeaderSaikyoDepature
-            station={station}
-            nextStation={nextStation}
-            line={line}
-            isLast={isLast}
-          />
-        );
-      }
       return (
         <HeaderSaikyo
           station={station}
