affadf30141131f4fb73777b7a8c1d053e30ebf4
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-06-30 03:51:12 +0900

    開発者限定のテーマ

--- src/screens/AppSettings/themes.ts
+++ src/screens/AppSettings/themes.ts
@@ -4,24 +4,29 @@ import { translate } from '../../translation';
 interface SettingsTheme {
   label: string;
   value: AppTheme;
+  devOnly: boolean;
 }
 
 const getSettingsThemes = (): SettingsTheme[] => [
   {
     label: translate('tokyoMetroLike'),
     value: AppTheme.TokyoMetro,
+    devOnly: false,
   },
   {
     label: translate('jrWestLike'),
     value: AppTheme.JRWest,
+    devOnly: true,
   },
   {
     label: translate('tyLike'),
     value: AppTheme.TY,
+    devOnly: false,
   },
   {
     label: translate('saikyoLineLike'),
     value: AppTheme.Saikyo,
+    devOnly: false,
   },
 ];
 
