748380a6218bd16f107396fbc01ca659f9f9b101
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-08-25 02:52:39 +0900

    TrainTypeBoxフォントサイズ再調整

--- src/components/TrainTypeBoxSaikyo.tsx
+++ src/components/TrainTypeBoxSaikyo.tsx
@@ -194,9 +194,9 @@ const TrainTypeBoxSaikyo: React.FC<Props> = ({
       (trainTypeText && trainTypeText.length > 6) ||
       trainTypeText?.includes('\n')
     ) {
-      return normalizeFontSize(12);
+      return normalizeFontSize(6);
     }
-    return normalizeFontSize(16);
+    return normalizeFontSize(8);
   }, [trainTypeText]);
 
   const prevFontSize = useValueRef(fontSize).current;
@@ -279,7 +279,7 @@ const TrainTypeBoxSaikyo: React.FC<Props> = ({
               {
                 ...styles.text,
                 fontSize,
-                lineHeight: Platform.OS === 'ios' ? undefined : fontSize + 2,
+                lineHeight: Platform.OS === 'ios' ? fontSize : fontSize + 2,
                 paddingLeft,
                 letterSpacing,
               },
@@ -293,8 +293,7 @@ const TrainTypeBoxSaikyo: React.FC<Props> = ({
               {
                 ...styles.text,
                 fontSize: prevFontSize,
-                lineHeight:
-                  Platform.OS === 'ios' ? undefined : prevFontSize + 2,
+                lineHeight: Platform.OS === 'ios' ? fontSize : prevFontSize + 2,
                 paddingLeft: prevPaddingLeft,
                 letterSpacing: prevLetterSpacing,
               },
