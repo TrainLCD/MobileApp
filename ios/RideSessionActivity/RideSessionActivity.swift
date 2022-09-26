546aecceb8b86e6422a77928cef805ceb439347f
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-09-26 21:14:22 +0900

    リファクタリング

--- ios/RideSessionActivity/RideSessionActivity.swift
+++ ios/RideSessionActivity/RideSessionActivity.swift
@@ -25,66 +25,60 @@ struct RideSessionWidget: Widget {
     } dynamicIsland: {context in
       DynamicIsland {
         DynamicIslandExpandedRegion(.leading) {
-          Group {
-            if (context.state.stopping) {
-              EmptyView()
-            } else {
-              VStack(alignment: .center) {
-                Text(context.state.stationName)
-                  .font(.callout)
+          if (context.state.stopping) {
+            EmptyView()
+          } else {
+            VStack(alignment: .center) {
+              Text(context.state.stationName)
+                .font(.callout)
+                .opacity(0.5)
+              if (!context.state.stationNumber.isEmpty) {
+                Text(getStationNumberText(context.state.stationNumber))
+                  .font(.caption)
                   .opacity(0.5)
-                if (!context.state.stationNumber.isEmpty) {
-                  Text(getStationNumberText(context.state.stationNumber))
-                    .font(.caption)
-                    .opacity(0.5)
-                }
               }
             }
           }
         }
         
         DynamicIslandExpandedRegion(.trailing) {
-          Group {
-            if (context.state.stopping) {
-              EmptyView()
-            } else {
-              VStack(alignment: .center) {
-                Text(context.state.nextStationName)
-                  .font(.callout)
+          if (context.state.stopping) {
+            EmptyView()
+          } else {
+            VStack(alignment: .center) {
+              Text(context.state.nextStationName)
+                .font(.callout)
+                .bold()
+              if (!context.state.nextStationNumber.isEmpty) {
+                Text(getStationNumberText(context.state.nextStationNumber))
+                  .font(.caption)
                   .bold()
-                if (!context.state.nextStationNumber.isEmpty) {
-                  Text(getStationNumberText(context.state.nextStationNumber))
-                    .font(.caption)
-                    .bold()
-                }
               }
             }
           }
         }
         
         DynamicIslandExpandedRegion(.center) {
-          Group {
-            if (context.state.stopping) {
-              VStack(alignment: .center ) {
-                Text(context.state.runningState)
-                  .bold()
+          if (context.state.stopping) {
+            VStack(alignment: .center ) {
+              Text(context.state.runningState)
+                .bold()
+                .font(.caption)
+              Text(context.state.stationName)
+                .bold()
+              if (!context.state.stationNumber.isEmpty) {
+                Text(getStationNumberText(context.state.stationNumber))
                   .font(.caption)
-                Text(context.state.stationName)
                   .bold()
-                if (!context.state.stationNumber.isEmpty) {
-                  Text(getStationNumberText(context.state.stationNumber))
-                    .font(.caption)
-                    .bold()
-                }
               }
-            } else {
-              VStack(alignment: .center) {
-                Text(context.state.runningState)
-                  .bold()
-                  .font(.caption)
-                Image(systemName: "arrow.right")
-                  .foregroundColor(.white)
-              }
+            }
+          } else {
+            VStack(alignment: .center) {
+              Text(context.state.runningState)
+                .bold()
+                .font(.caption)
+              Image(systemName: "arrow.right")
+                .foregroundColor(.white)
             }
           }
         }
@@ -97,53 +91,57 @@ struct RideSessionWidget: Widget {
           .font(.caption)
           .bold()
       } compactTrailing: {
-        Group {
-          if (context.state.stopping) {
-            VStack {
-              Text(context.state.stationName)
+        if (context.state.stopping) {
+          VStack {
+            Text(context.state.stationName)
+              .font(.caption)
+              .bold()
+            if (!context.state.stationNumber.isEmpty) {
+              Text(getStationNumberText(context.state.stationNumber))
                 .font(.caption)
                 .bold()
-              if (!context.state.stationNumber.isEmpty) {
-                Text(getStationNumberText(context.state.stationNumber))
-                  .font(.caption)
-                  .bold()
-              }
-            }
-          } else {
-            VStack {
-              Text(context.state.nextStationName)
-                .font(.caption)
-                .bold()
-              if (!context.state.nextStationNumber.isEmpty) {
-                Text(getStationNumberText(context.state.nextStationNumber))
-                  .font(.caption)
-                  .bold()
-              }
             }
           }
-        }.frame(
-          minWidth: 0,
-          maxWidth: .infinity,
-          minHeight: 0,
-          maxHeight: .infinity,
-          alignment: .center
-        )
+          .frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            minHeight: 0,
+            maxHeight: .infinity,
+            alignment: .center
+          )
+        } else {
+          VStack {
+            Text(context.state.nextStationName)
+              .font(.caption)
+              .bold()
+            if (!context.state.nextStationNumber.isEmpty) {
+              Text(getStationNumberText(context.state.nextStationNumber))
+                .font(.caption)
+                .bold()
+            }
+          }
+          .frame(
+            minWidth: 0,
+            maxWidth: .infinity,
+            minHeight: 0,
+            maxHeight: .infinity,
+            alignment: .center
+          )
+        }
       } minimal: {
-        Group {
-          if (context.state.stopping) {
-            VStack(alignment: .center) {
-              Image(systemName: "stop.circle")
-              Text(context.state.stationName)
-                .multilineTextAlignment(.center)
-                .font(.caption)
-            }
-          } else {
-            VStack(alignment: .center) {
-              Image(systemName: "play.circle")
-              Text(context.state.nextStationName)
-                .multilineTextAlignment(.center)
-                .font(.caption)
-            }
+        if (context.state.stopping) {
+          VStack(alignment: .center) {
+            Image(systemName: "stop.circle")
+            Text(context.state.stationName)
+              .multilineTextAlignment(.center)
+              .font(.caption)
+          }
+        } else {
+          VStack(alignment: .center) {
+            Image(systemName: "play.circle")
+            Text(context.state.nextStationName)
+              .multilineTextAlignment(.center)
+              .font(.caption)
           }
         }
       }
@@ -153,7 +151,7 @@ struct RideSessionWidget: Widget {
 
 struct LockScreenLiveActivityView: View {
   let context: ActivityViewContext<RideSessionAttributes>
-
+  
   var body: some View {
     Group {
       if (context.state.stopping) {
@@ -161,15 +159,15 @@ struct LockScreenLiveActivityView: View {
           Text(context.state.runningState)
             .bold()
             .font(.caption)
-            VStack {
-              Text(context.state.stationName)
+          VStack {
+            Text(context.state.stationName)
+              .bold()
+            if (!context.state.stationNumber.isEmpty) {
+              Text(getStationNumberText(context.state.stationNumber))
+                .font(.caption)
                 .bold()
-              if (!context.state.stationNumber.isEmpty) {
-                Text(getStationNumberText(context.state.stationNumber))
-                  .font(.caption)
-                  .bold()
-              }
             }
+          }
           .frame(minWidth: 0, maxWidth: .infinity)
         }
       } else {
