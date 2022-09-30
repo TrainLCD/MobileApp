9e1bf35b2599d70821561a74bbd82428938c1b21
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2022-09-30 23:17:45 +0900

    timeInterval除名

--- src/constants/location.ts
+++ src/constants/location.ts
@@ -1,5 +1,3 @@
 export const LOCATION_TASK_NAME = 'trainlcd-background-location-task';
-// 位置情報更新間隔(msec)
-export const LOCATION_UPDATE_THROTTLE_INTERVAL = 5 * 1000;
 // 端末上の駅直線距離計算誤差(meter)
 export const COMPUTE_DISTANCE_ACCURACY = 100;
