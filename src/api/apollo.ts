3827b883deb4b740ea6bd88f79dfb74ebbfc6915
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-01-09 07:19:53 +0900

    乗り換えデータキャシュキー修正

--- src/api/apollo.ts
+++ src/api/apollo.ts
@@ -1,4 +1,10 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+/* eslint-disable no-underscore-dangle */
+import {
+  ApolloClient,
+  defaultDataIdFromObject,
+  InMemoryCache,
+} from '@apollo/client';
+import { Station } from '../models/StationAPI';
 
 const NON_PROD_API_URL = __DEV__
   ? process.env.API_URL_DEV
@@ -7,7 +13,25 @@ const NON_PROD_API_URL = __DEV__
 const getClient = (dev: boolean): ApolloClient<unknown> =>
   new ApolloClient({
     uri: dev ? NON_PROD_API_URL : process.env.API_URL_PROD,
-    cache: new InMemoryCache(),
+    cache: new InMemoryCache({
+      dataIdFromObject(responseObject) {
+        // eslint-disable-next-line no-underscore-dangle
+        switch (responseObject.__typename) {
+          case 'Station':
+            return `${responseObject.__typename}_${responseObject.groupId}_${responseObject.id}`;
+          case 'Line':
+            if (responseObject.transferStation) {
+              return `${responseObject.__typename}_${responseObject.id}_${
+                (responseObject.transferStation as Station).id
+              }`;
+            }
+            return `${responseObject.__typename}_${responseObject.id}`;
+
+          default:
+            return defaultDataIdFromObject(responseObject);
+        }
+      },
+    }),
   });
 
 export default getClient;
