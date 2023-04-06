3d60a69281c9a25df671ab8cd890f5c893e8c767
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-04-06 11:55:20 +0900

    次の路線の列車種別を正しく取得できていないバグを修正

---
+++ src/hooks/useNextTrainType.ts
@@ -0,0 +1,22 @@
+import { useMemo } from 'react';
+import { useRecoilValue } from 'recoil';
+import { APITrainType, APITrainTypeMinimum } from '../models/StationAPI';
+import navigationState from '../store/atoms/navigation';
+import useConnectedLines from './useConnectedLines';
+
+const useNextTrainType = (): APITrainTypeMinimum | null => {
+  const { trainType } = useRecoilValue(navigationState);
+  const connectedLines = useConnectedLines();
+  const typedTrainType = trainType as APITrainType;
+  const nextTrainType = useMemo(() => {
+    const nextLine = connectedLines[0];
+    return (
+      typedTrainType.allTrainTypes.find((tt) => tt.line.id === nextLine?.id) ??
+      null
+    );
+  }, [connectedLines, typedTrainType.allTrainTypes]);
+
+  return nextTrainType;
+};
+
+export default useNextTrainType;
