827cd2b2b1d3ca14a39cb974835c8031bbe60272
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-04-15 10:52:31 +0900

    多分不要になった処理の削除

--- src/components/HeaderSaikyo.tsx
+++ src/components/HeaderSaikyo.tsx
@@ -11,7 +11,6 @@ import Animated, {
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useRecoilValue } from 'recoil';
-import { v3 as uuidv3 } from 'uuid';
 import { STATION_NAME_FONT_SIZE } from '../constants';
 import { MARK_SHAPE } from '../constants/numbering';
 import useAppState from '../hooks/useAppState';
@@ -88,7 +87,6 @@ const styles = StyleSheet.create({
   },
   stationNameContainer: {
     position: 'absolute',
-    flexDirection: 'row',
     justifyContent: 'center',
   },
   stationName: {
@@ -736,28 +734,19 @@ const HeaderSaikyo: React.FC<CommonHeaderProps> = ({
                   transform: [{ scaleX: stationNameScale }],
                 }}
               >
-                {Array.from({ length: stationText.length })
-                  .fill(null)
-                  .map((_, i) => ({
-                    char: stationText[i],
-                    key: uuidv3(`${i}${stationText[i]}`, uuidv3.URL),
-                  }))
-                  .map((obj) => (
-                    <Animated.Text
-                      key={obj.key}
-                      style={[
-                        getTopNameAnimatedStyles(),
-                        styles.stationName,
-                        {
-                          opacity: nameFadeAnim,
-                          minHeight: STATION_NAME_FONT_SIZE,
-                          fontSize: STATION_NAME_FONT_SIZE,
-                        },
-                      ]}
-                    >
-                      {obj.char}
-                    </Animated.Text>
-                  ))}
+                <Animated.Text
+                  style={[
+                    getTopNameAnimatedStyles(),
+                    styles.stationName,
+                    {
+                      opacity: nameFadeAnim,
+                      minHeight: STATION_NAME_FONT_SIZE,
+                      fontSize: STATION_NAME_FONT_SIZE,
+                    },
+                  ]}
+                >
+                  {stationText}
+                </Animated.Text>
               </View>
 
               <View
@@ -766,30 +755,21 @@ const HeaderSaikyo: React.FC<CommonHeaderProps> = ({
                   transform: [{ scaleX: prevStationNameScale }],
                 }}
               >
-                {Array.from({ length: prevStationText.length })
-                  .fill(null)
-                  .map((_, i) => ({
-                    char: prevStationText[i],
-                    key: uuidv3(`${i}${prevStationText[i]}`, uuidv3.URL),
-                  }))
-                  .map((obj) => (
-                    <Animated.Text
-                      key={obj.key}
-                      style={[
-                        styles.stationName,
-                        getBottomNameAnimatedStyles(),
-                        {
-                          opacity: nameFadeAnim.interpolate({
-                            inputRange: [0, 1],
-                            outputRange: [1, 0],
-                          }),
-                          fontSize: STATION_NAME_FONT_SIZE,
-                        },
-                      ]}
-                    >
-                      {obj.char}
-                    </Animated.Text>
-                  ))}
+                <Animated.Text
+                  style={[
+                    styles.stationName,
+                    getBottomNameAnimatedStyles(),
+                    {
+                      opacity: nameFadeAnim.interpolate({
+                        inputRange: [0, 1],
+                        outputRange: [1, 0],
+                      }),
+                      fontSize: STATION_NAME_FONT_SIZE,
+                    },
+                  ]}
+                >
+                  {prevStationText}
+                </Animated.Text>
               </View>
             </View>
           </View>
