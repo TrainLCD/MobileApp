1c13acbdd3f238b53aceb234d3b2a7bb3c0dd61b
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-05-18 13:40:02 +0900

    LineBoardSaikyoでの低解像度タブレット対応

--- src/components/LineBoardSaikyo.tsx
+++ src/components/LineBoardSaikyo.tsx
@@ -23,7 +23,9 @@ import { Line, Station } from '../models/StationAPI';
 import lineState from '../store/atoms/line';
 import stationState from '../store/atoms/station';
 import getStationNameR from '../utils/getStationNameR';
+import isFullSizedTablet from '../utils/isFullSizedTablet';
 import getIsPass from '../utils/isPass';
+import isSmallTablet from '../utils/isSmallTablet';
 import isTablet from '../utils/isTablet';
 import omitJRLinesIfThresholdExceeded from '../utils/jr';
 import { heightScale, widthScale } from '../utils/scale';
@@ -40,7 +42,7 @@ const useBarStyles = ({
   index?: number;
 }): { left: number; width: number } => {
   const left = useMemo(() => {
-    if (Platform.OS === 'android' && !isTablet) {
+    if (isFullSizedTablet) {
       if (index === 0) {
         return widthScale(-32);
       }
@@ -67,7 +69,7 @@ const useBarStyles = ({
       if (!hasNotch() && Platform.OS === 'ios') {
         return widthScale(62);
       }
-      if (Platform.OS === 'android' && !isTablet) {
+      if (isFullSizedTablet) {
         return widthScale(58);
       }
       return widthScale(62);
@@ -75,7 +77,7 @@ const useBarStyles = ({
     if (!hasNotch() && Platform.OS === 'ios') {
       return widthScale(62);
     }
-    if (Platform.OS === 'android' && !isTablet) {
+    if (isFullSizedTablet) {
       return widthScale(58);
     }
     return widthScale(62);
@@ -112,21 +114,41 @@ const getBarTerminalRight = (): number => {
   if (isTablet) {
     return -42;
   }
-  if (Platform.OS === 'android' && !isTablet) {
+  if (isFullSizedTablet) {
     return -26;
   }
   return -31;
 };
 
+const barBottom = ((): number => {
+  if (isFullSizedTablet) {
+    return -52;
+  }
+  if (isSmallTablet) {
+    return 30;
+  }
+  return 32;
+})();
+
+const barTerminalBottom = ((): number => {
+  if (isFullSizedTablet) {
+    return -54;
+  }
+  if (isSmallTablet) {
+    return 28;
+  }
+  return 32;
+})();
+
 const styles = StyleSheet.create({
   root: {
     flex: 1,
     height: screenHeight,
-    bottom: isTablet ? screenHeight / 2.5 : undefined,
+    bottom: isFullSizedTablet ? screenHeight / 2.5 : undefined,
   },
   bar: {
     position: 'absolute',
-    bottom: isTablet ? -52 : 32,
+    bottom: barBottom,
     height: isTablet ? 48 : 32,
   },
   barTerminal: {
@@ -134,11 +156,11 @@ const styles = StyleSheet.create({
     height: isTablet ? 53 : 32,
     position: 'absolute',
     right: getBarTerminalRight(),
-    bottom: isTablet ? -54 : 32,
+    bottom: barTerminalBottom,
   },
   stationNameWrapper: {
     flexDirection: 'row',
-    justifyContent: isTablet ? 'flex-start' : undefined,
+    justifyContent: isFullSizedTablet ? 'flex-start' : undefined,
     marginLeft: 32,
     flex: 1,
   },
@@ -147,7 +169,7 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     justifyContent: 'flex-end',
     bottom: isTablet ? 84 : undefined,
-    paddingBottom: !isTablet ? 84 : undefined,
+    paddingBottom: !isFullSizedTablet ? 84 : undefined,
   },
   stationName: {
     textAlign: 'center',
@@ -155,7 +177,7 @@ const styles = StyleSheet.create({
     lineHeight: RFValue(stationNameLineHeight),
     fontWeight: 'bold',
     color: '#3a3a3a',
-    marginLeft: isTablet ? 10 : 5,
+    marginLeft: isFullSizedTablet ? 10 : 5,
   },
   stationNameEn: {
     fontSize: RFValue(18),
@@ -181,7 +203,7 @@ const styles = StyleSheet.create({
     height: isTablet ? 36 : 24,
     position: 'absolute',
     zIndex: 9999,
-    bottom: isTablet ? -46 : 32 + 4,
+    bottom: isFullSizedTablet ? -46 : 32 + 4,
     overflow: 'visible',
   },
   chevron: {
