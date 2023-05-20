1613bc3aa4aa26eb6439194c66752935efdea489
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-05-20 14:53:40 +0900

    一旦ウェアラブルもv5.0.0

--- android/wearable/build.gradle.kts
+++ android/wearable/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 30
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "5.0.0"
         vectorDrawables {
             useSupportLibrary = true
         }
