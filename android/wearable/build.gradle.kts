d003850810ee6c9092a004e693f60dbca25e7fbe
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-05-20 19:56:03 +0900

    bump version

--- android/wearable/build.gradle.kts
+++ android/wearable/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "me.tinykitten.trainlcd"
         minSdk = 30
         targetSdk = 33
-        versionCode = 696
+        versionCode = 698
         versionName = "5.0.0"
         vectorDrawables {
             useSupportLibrary = true
