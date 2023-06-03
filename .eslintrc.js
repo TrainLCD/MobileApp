edea2d2f17d208f8f12808d275f49c2faa074ae9
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-06-03 12:39:43 +0900

    eslint最初期化

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  rules: {},
+};
