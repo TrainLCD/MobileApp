99b7e1d7429c90c64ef336a0f8b3e864988eefa4
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-06-03 12:36:16 +0900

    yarn失敗するのでwdyr削除

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import { registerRootComponent } from 'expo';
 import 'react-native-get-random-values';
 import 'react-native-url-polyfill/auto';
-import './wdyr';
 
 import App from './App';
 
--- src/@types/env.d.tsx
+++ src/@types/env.d.tsx
@@ -4,7 +4,6 @@ declare module 'react-native-dotenv' {
   export const GOOGLE_API_KEY: string;
   export const NEARBY_STATIONS_LIMIT: string;
   export const MIRRORING_SHARE_DEEPLINK_URL: string;
-  export const ENABLE_WDYR: 'true' | 'false';
   // production environment only
   export const DEV_MODE_API_URL: string | undefined;
 }
