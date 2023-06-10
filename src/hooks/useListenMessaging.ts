97a0fc1cb198461f0381d652a29b4d583de4b14b
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-06-11 07:58:54 +0900

    FCMのパーミッション要求

--- src/hooks/useListenMessaging.ts
+++ src/hooks/useListenMessaging.ts
@@ -1,9 +1,17 @@
 import messaging from '@react-native-firebase/messaging'
 import { useEffect } from 'react'
-import { Alert } from 'react-native'
+import { Alert, PermissionsAndroid } from 'react-native'
 
 const useListenMessaging = () => {
   useEffect(() => {
+    const requestPermissionAsync = async () => {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+      )
+      await messaging().requestPermission()
+    }
+    requestPermissionAsync()
+
     const unsubscribe = messaging().onMessage(async (remoteMessage) =>
       Alert.alert(
         remoteMessage.notification?.title ?? '',
