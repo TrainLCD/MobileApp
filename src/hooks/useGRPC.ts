41b169102401933fdadc8dc16793e6fb39aa8f00
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-07-23 10:52:45 +0900

    APIのURLをRemote Configから取るのをやめた

--- src/hooks/useGRPC.ts
+++ src/hooks/useGRPC.ts
@@ -1,25 +1,25 @@
 import { useEffect, useMemo } from 'react'
-import { DEV_API_URL } from 'react-native-dotenv'
+import {
+  DEV_API_URL,
+  PRODUCTION_API_URL,
+  STAGING_API_URL,
+} from 'react-native-dotenv'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import { StationAPIClient } from '../gen/StationapiServiceClientPb'
 import devState from '../store/atoms/dev'
 import grpcState from '../store/atoms/grpc'
-import useRemoteConfig from '../utils/useRemoteConfig'
 
 const useGRPC = () => {
   const [{ cachedClient }, setGRPC] = useRecoilState(grpcState)
   const { devMode } = useRecoilValue(devState)
-  const {
-    config: { production_grpc_url, staging_grpc_url },
-  } = useRemoteConfig()
 
   const apiUrl = useMemo(() => {
     if (__DEV__) {
       return DEV_API_URL
     }
 
-    return devMode ? staging_grpc_url : production_grpc_url
-  }, [devMode, production_grpc_url, staging_grpc_url])
+    return devMode ? STAGING_API_URL : PRODUCTION_API_URL
+  }, [devMode])
 
   useEffect(() => {
     if (cachedClient || !apiUrl) {
