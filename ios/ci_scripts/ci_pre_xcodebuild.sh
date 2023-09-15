83f24abf79f43b2a67efa2694c4fdbc8b9d35db4
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-09-15 16:18:17 +0900

    mkdir

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+mkdir -p /Volumes/workspace/repository/ios/Schemes/Dev
 echo $GOOGLE_SERVICE_INFO_PLIST > /Volumes/workspace/repository/ios/Schemes/Dev/GoogleService-Info.plist
 echo $DOTENV_LOCAL > /Volumes/workspace/repository/.env.local
