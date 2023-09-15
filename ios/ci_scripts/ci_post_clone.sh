ab64891f6e0805f90baa6395e8c0d39defb3d91b
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-09-15 17:19:43 +0900

    結局使わなかったコメントアウトの行を削除

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,5 +6,3 @@ brew link node@18
 
 npm i
 pod install
-# the sed command from RN cant find the file... so we have to run it ourselves
-# sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
