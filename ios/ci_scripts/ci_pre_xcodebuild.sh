74ec88afba8d0c0f4ff8dc5d39cdb899402d4e41
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-09-15 15:42:39 +0900

    絶対パス

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo $GOOGLE_SERVICE_INFO_PLIST > ../Schemes/Dev/GoogleService-Info.plist
-echo $DOTENV_LOCAL > ./.env.local
+echo $GOOGLE_SERVICE_INFO_PLIST > /Volumes/workspace/repository/ios/Schemes/Dev/GoogleService-Info.plist
+echo $DOTENV_LOCAL > /Volumes/workspace/repository/.env.local
