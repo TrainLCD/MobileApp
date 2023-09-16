9de8d49c429c45b475e2a90a21908a4c38a15cd6
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-09-16 11:16:02 +0900

    .env.localの場所を改めてみる

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -2,3 +2,4 @@
 
 mkdir -p /Volumes/workspace/repository/ios/Schemes/Dev
 echo $GOOGLE_SERVICE_INFO_PLIST > /Volumes/workspace/repository/ios/Schemes/Dev/GoogleService-Info.plist
+echo $DOTENV_LOCAL > ../.env.local
