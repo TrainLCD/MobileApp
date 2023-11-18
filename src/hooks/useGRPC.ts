97641b8af530b489b10d985de6e08960864660d2
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-11-18 11:28:48 +0900

    bump android version

--- src/hooks/useGRPC.ts
+++ src/hooks/useGRPC.ts
@@ -1,21 +1,17 @@
 import { useEffect, useMemo } from 'react'
-import {
-  DEV_API_URL,
-  PRODUCTION_API_URL,
-  STAGING_API_URL,
-} from 'react-native-dotenv'
+import { PRODUCTION_API_URL, STAGING_API_URL } from 'react-native-dotenv'
 import { useRecoilState } from 'recoil'
 import { StationAPIClient } from '../gen/StationapiServiceClientPb'
-import { isDevApp } from '../utils/isDevApp'
 import cacheState from '../store/atoms/cache'
+import { isDevApp } from '../utils/isDevApp'
 
 const useGRPC = () => {
   const [{ grpcClient }, setCacheState] = useRecoilState(cacheState)
 
   const apiUrl = useMemo(() => {
-    if (__DEV__) {
-      return DEV_API_URL
-    }
+    // if (__DEV__) {
+    //   return DEV_API_URL
+    // }
 
     return isDevApp ? STAGING_API_URL : PRODUCTION_API_URL
   }, [])
