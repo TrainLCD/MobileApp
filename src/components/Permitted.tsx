2fe4c21fceb977350353cecfb2ae3e55d0d8a8fd
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-11-29 07:04:31 +0900

    useTTS()を１回のみ

--- src/components/Permitted.tsx
+++ src/components/Permitted.tsx
@@ -29,7 +29,6 @@ import useListenMessaging from '../hooks/useListenMessaging'
 import useReport from '../hooks/useReport'
 import useReportEligibility from '../hooks/useReportEligibility'
 import useResetMainState from '../hooks/useResetMainState'
-import useTTS from '../hooks/useTTS'
 import useUpdateLiveActivities from '../hooks/useUpdateLiveActivities'
 import { APP_THEME, AppTheme } from '../models/Theme'
 import locationState from '../store/atoms/location'
@@ -96,8 +95,6 @@ const PermittedLayout: React.FC<Props> = ({ children }: Props) => {
   useAndroidWearable()
   useUpdateLiveActivities()
   useListenMessaging()
-  // QAチーム専用
-  useTTS()
   // 実験用
   useBLE()
 
