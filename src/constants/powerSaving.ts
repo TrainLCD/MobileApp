4644a4243af8430b07f515413760209dd180be20
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-11-30 12:09:22 +0900

    locationServiceDistanceFilter定数化

--- src/constants/powerSaving.ts
+++ src/constants/powerSaving.ts
@@ -3,6 +3,9 @@ import {
   COMPUTE_DISTANCE_ACCURACY_HIGH,
   COMPUTE_DISTANCE_ACCURACY_LOW,
   COMPUTE_DISTANCE_ACCURACY_NORMAL,
+  DISTANCE_FILTER_BALANCED,
+  DISTANCE_FILTER_HIGH,
+  DISTANCE_FILTER_LOW,
 } from './location'
 
 export const POWER_SAVING_PRESETS = {
@@ -27,26 +30,26 @@ export const POWER_SETTING_VALUES: Record<PowerSavingPreset, PowerPresetValue> =
     [POWER_SAVING_PRESETS.LOWEST]: {
       locationServiceAccuracy: LocationAccuracy.Low,
       computeDistanceAccuracy: COMPUTE_DISTANCE_ACCURACY_LOW,
-      locationServiceDistanceFilter: 100,
+      locationServiceDistanceFilter: DISTANCE_FILTER_LOW,
     },
     [POWER_SAVING_PRESETS.LOW]: {
       locationServiceAccuracy: LocationAccuracy.Balanced,
       computeDistanceAccuracy: COMPUTE_DISTANCE_ACCURACY_LOW,
-      locationServiceDistanceFilter: 100,
+      locationServiceDistanceFilter: DISTANCE_FILTER_LOW,
     },
     [POWER_SAVING_PRESETS.BALANCED]: {
       locationServiceAccuracy: LocationAccuracy.Balanced,
       computeDistanceAccuracy: COMPUTE_DISTANCE_ACCURACY_NORMAL,
-      locationServiceDistanceFilter: 50,
+      locationServiceDistanceFilter: DISTANCE_FILTER_BALANCED,
     },
     [POWER_SAVING_PRESETS.HIGH]: {
       locationServiceAccuracy: LocationAccuracy.High,
       computeDistanceAccuracy: COMPUTE_DISTANCE_ACCURACY_NORMAL,
-      locationServiceDistanceFilter: 10,
+      locationServiceDistanceFilter: DISTANCE_FILTER_HIGH,
     },
     [POWER_SAVING_PRESETS.HIGHEST]: {
       locationServiceAccuracy: LocationAccuracy.High,
       computeDistanceAccuracy: COMPUTE_DISTANCE_ACCURACY_HIGH,
-      locationServiceDistanceFilter: 10,
+      locationServiceDistanceFilter: DISTANCE_FILTER_HIGH,
     },
   } as const
