8c124ee9e41e3bcd6d23dbb4c3284ea9145078cf
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-12-03 05:06:22 +0900

    6.0.1

--- android/wearable/build.gradle.kts
+++ android/wearable/build.gradle.kts
@@ -12,8 +12,8 @@ android {
       minSdk = 30
       targetSdk = 33
       // 10203011 <- 10203(v1.2.3 version name)+01(build number)+1(Wearable app)
-      versionCode = 60000311
-      versionName = "6.0.0"
+      versionCode = 60001001
+      versionName = "6.0.1"
       vectorDrawables {
           useSupportLibrary = true
       }
