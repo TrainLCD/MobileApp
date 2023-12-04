42f3a37eff4c670a92d4e4cb59021fb61bdeabd4
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-12-05 07:22:29 +0900

    Xcode Cloudビルド失敗修正

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
-brew install cocoapods node@18 protobuf
-brew link node@18
+brew update
+brew install cocoapods node@20 protobuf
+brew link node@20
 
 npm i
 pod install
