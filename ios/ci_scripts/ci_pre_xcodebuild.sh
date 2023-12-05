782238975a4543ef6179a237176ff6a2448284bd
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-12-06 03:34:08 +0900

    ci_pre_xcodebuildにまとめる

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+brew install cocoapods node protobuf
+
+npm install
+pod install
+
 mkdir -p /Volumes/workspace/repository/ios/Schemes/Dev
 echo $GOOGLE_SERVICE_INFO_PLIST > /Volumes/workspace/repository/ios/Schemes/Dev/GoogleService-Info.plist
 echo $DOTENV_LOCAL > /Volumes/workspace/repository/.env.local
