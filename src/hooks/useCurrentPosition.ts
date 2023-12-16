c54c6b199da75c61d5648d2b7b634985d353bfd3
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-12-17 07:41:03 +0900

    GoogleのGeolocation APIを使わない

--- src/hooks/useCurrentPosition.ts
+++ src/hooks/useCurrentPosition.ts
@@ -1,29 +1,15 @@
 import * as Location from 'expo-location'
 import { useCallback } from 'react'
-import { FETCH_LOCATION_TIMEOUT } from '../constants'
-import { sleep } from '../utils/sleep'
-import { useGoogleGeolocation } from './useGoogleGeolocation'
 
 export const useCurrentPosition = () => {
-  const { fetchLocationAndroid } = useGoogleGeolocation()
-
   const getCurrentPositionAsync =
     useCallback(async (): Promise<Location.LocationObject | null> => {
-      const expoLocationPromise = Location.getCurrentPositionAsync({
+      const pos = await Location.getCurrentPositionAsync({
         accuracy: Location.Accuracy.Balanced,
       })
-      const sleepPromise = sleep(FETCH_LOCATION_TIMEOUT)
-      const nullishPosition = await Promise.race([
-        expoLocationPromise,
-        sleepPromise,
-      ])
 
-      if (nullishPosition) {
-        return nullishPosition
-      }
-
-      return fetchLocationAndroid()
-    }, [fetchLocationAndroid])
+      return pos
+    }, [])
 
   return { getCurrentPositionAsync }
 }
