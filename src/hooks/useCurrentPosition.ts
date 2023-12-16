24c2bb829d1ff45cb092906391235bb44514c154
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2023-12-17 07:48:19 +0900

    冗長なコードの修正

--- src/hooks/useCurrentPosition.ts
+++ src/hooks/useCurrentPosition.ts
@@ -2,14 +2,13 @@ import * as Location from 'expo-location'
 import { useCallback } from 'react'
 
 export const useCurrentPosition = () => {
-  const getCurrentPositionAsync =
-    useCallback(async (): Promise<Location.LocationObject | null> => {
-      const pos = await Location.getCurrentPositionAsync({
+  const getCurrentPositionAsync = useCallback(
+    () =>
+      Location.getCurrentPositionAsync({
         accuracy: Location.Accuracy.Balanced,
-      })
-
-      return pos
-    }, [])
+      }),
+    []
+  )
 
   return { getCurrentPositionAsync }
 }
