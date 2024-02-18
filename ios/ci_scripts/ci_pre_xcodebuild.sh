508481457e5985196796b37aa935209b172a799f
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-02-18 21:03:13 +0900

    ci_post_clone.shに切り分け

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-brew install cocoapods node@20
-brew link node@20
-
-npm ci
-pod install
-
 mkdir -p /Volumes/workspace/repository/ios/Schemes/Dev
 echo $GOOGLE_SERVICE_INFO_PLIST > /Volumes/workspace/repository/ios/Schemes/Dev/GoogleService-Info.plist
 echo $DOTENV_LOCAL > /Volumes/workspace/repository/.env.local
