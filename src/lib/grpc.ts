45239eb0afa955d08c33894ee6ba7a6b39af1d88
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-03-07 15:22:11 +0900

    grpc clientはcustom hookである必要がない

---
+++ src/lib/grpc.ts
@@ -0,0 +1,24 @@
+import { createPromiseClient } from '@connectrpc/connect'
+import {
+  DEV_API_URL,
+  PRODUCTION_API_URL,
+  STAGING_API_URL,
+} from 'react-native-dotenv'
+import { StationAPI } from '../../gen/proto/stationapi_connect'
+import { createXHRGrpcWebTransport } from '../utils/customTransport'
+import { isDevApp } from '../utils/isDevApp'
+
+const baseUrl = (() => {
+  if (__DEV__) {
+    return DEV_API_URL
+  }
+
+  return isDevApp ? STAGING_API_URL : PRODUCTION_API_URL
+})()
+
+export const grpcClient = createPromiseClient(
+  StationAPI,
+  createXHRGrpcWebTransport({
+    baseUrl,
+  })
+)
