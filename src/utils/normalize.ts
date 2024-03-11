b9c22d285a9bcad575b5660b8158d7d3544c2402
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-03-12 00:02:57 +0900

    ローマ字の正規化で空白も無視する

--- src/utils/normalize.ts
+++ src/utils/normalize.ts
@@ -2,11 +2,14 @@ export const normalizeRomanText = (str: string): string => {
   const isAllCaps = str
     .slice(1)
     .split('')
-    .filter((s) => s !== '-')
+    .filter((s) => s !== '-' && s !== ' ')
     .every((s) => /[A-Z]/.test(s))
   if (isAllCaps) {
-    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+    return (
+      str.replace('JR', 'J-R').charAt(0).toUpperCase() +
+      str.slice(1).toLowerCase()
+    )
   }
 
-  return str
+  return str.replace('JR', 'J-R')
 }
