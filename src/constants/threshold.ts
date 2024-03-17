d8a2b773d8b5833ce7273416ac2f02bea17cafdf
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-03-17 08:24:20 +0000

    到着最大閾値の拡大

--- src/constants/threshold.ts
+++ src/constants/threshold.ts
@@ -6,7 +6,7 @@ export const DEFAULT_HEADER_TRANSITION_DELAY = 500 // ms
 export const LONG_PRESS_DURATION = 500
 // 閾値
 export const APPROACHING_BASE_THRESHOLD = 1000
-export const ARRIVED_BASE_THRESHOLD = 300
+export const ARRIVED_BASE_THRESHOLD = 500
 export const MANY_LINES_THRESHOLD = 7
 export const OMIT_JR_THRESHOLD = 3 // これ以上JR線があったら「JR線」で省略しよう
 export const JR_LINE_MAX_ID = 6
