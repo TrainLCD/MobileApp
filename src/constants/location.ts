dbf05e59fe21a1771c6bae1eb09ad250814005e0
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-03-17 04:55:25 +0000

    distanceIntervalにもどし

--- src/constants/location.ts
+++ src/constants/location.ts
@@ -3,5 +3,5 @@ export const LOCATION_TASK_NAME = 'background-location-task'
 export const COMPUTE_DISTANCE_ACCURACY_NORMAL = 10
 export const COMPUTE_DISTANCE_ACCURACY_HIGH = 1
 // 位置情報の処理間隔(meter)
-export const DISTANCE_FILTER_BALANCED = 200
+export const DISTANCE_FILTER_BALANCED = 200 // TODO: 仮置きなので要検証
 export const DISTANCE_FILTER_HIGH = 100
