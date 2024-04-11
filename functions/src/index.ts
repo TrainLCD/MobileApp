9d1c942a83c4399e1e48c6acbd8b6c332b981acb
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-04-11 11:37:38 +0900

    TTSの処理をFirebaseに寄せた

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import { XMLParser } from "fast-xml-parser";
 import * as admin from "firebase-admin";
 import { initializeApp } from "firebase-admin/app";
 import * as functions from "firebase-functions";
+import { HttpsError } from "firebase-functions/v1/auth";
 import { AppStoreReviewFeed, AppStoreReviewsDoc } from "./models/appStoreFeed";
 import { DiscordEmbed } from "./models/common";
 import { Report } from "./models/feedback";
@@ -323,4 +324,153 @@ exports.detectHourlyAppStoreNewReview = functions.pubsub
     });
 
     return null;
+  });
+
+exports.tts = functions
+  .region("asia-northeast1")
+  .https.onCall(async (data, ctx) => {
+    if (!ctx.auth) {
+      throw new HttpsError(
+        "failed-precondition",
+        "The function must be called while authenticated.",
+      );
+    }
+
+    const ssmlJa = data.ssmlJa;
+    if (!(typeof ssmlJa === "string") || ssmlJa.length === 0) {
+      throw new HttpsError(
+        "invalid-argument",
+        `The function must be called with one arguments "ssmlJa" containing the message ssmlJa to add.`,
+      );
+    }
+    const ssmlEn = data.ssmlEn;
+    if (!(typeof ssmlEn === "string") || ssmlEn.length === 0) {
+      throw new HttpsError(
+        "invalid-argument",
+        `The function must be called with one arguments "ssmlEn" containing the message ssmlEn to add.`,
+      );
+    }
+    const isPremium = data.premium;
+    const jaVoiceName = isPremium ? "ja-JP-Neural2-B" : "ja-JP-Standard-B";
+    const enVoiceName = isPremium ? "en-US-Neural2-G" : "en-US-Standard-G";
+
+    const jaCollection = firestore
+      .collection("caches")
+      .doc("tts")
+      .collection("ja");
+    const enCollection = firestore
+      .collection("caches")
+      .doc("tts")
+      .collection("en");
+
+    // TODO: キャッシュ復元処理
+    const jaSnapshot = await jaCollection
+      .where("ssml", "==", ssmlJa)
+      .where("voice", "==", jaVoiceName)
+      .get();
+    const enSnapshot = await enCollection
+      .where("ssml", "==", ssmlEn)
+      .where("voice", "==", enVoiceName)
+      .get();
+
+    if (!jaSnapshot.empty || !enSnapshot.empty) {
+      const jaAudioData =
+        (!jaSnapshot.empty &&
+          (await storage
+            .bucket()
+            .file(jaSnapshot.docs[0]?.data().path)
+            .download())) ||
+        null;
+      const enAudioData =
+        (!enSnapshot.empty &&
+          (await storage
+            .bucket()
+            .file(enSnapshot.docs[0]?.data().path)
+            .download())) ||
+        null;
+
+      const jaAudioContent = jaAudioData?.[0]?.toString("base64") || null;
+      const enAudioContent = enAudioData?.[0]?.toString("base64") || null;
+
+      return { jaAudioContent, enAudioContent };
+    }
+    const ttsUrl = `https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=${process.env.GOOGLE_TTS_API_KEY}`;
+
+    const reqBodyJa = {
+      input: {
+        ssml: ssmlJa,
+      },
+      voice: {
+        languageCode: "ja-JP",
+        name: jaVoiceName,
+      },
+      audioConfig: {
+        audioEncoding: isPremium ? "LINEAR16" : "MP3",
+      },
+    };
+
+    const reqBodyEn = {
+      input: {
+        ssml: ssmlEn,
+      },
+      voice: {
+        languageCode: "en-US",
+        name: enVoiceName,
+      },
+      audioConfig: {
+        audioEncoding: isPremium ? "LINEAR16" : "MP3",
+      },
+    };
+
+    const { audioContent: jaAudioContent } = await (
+      await fetch(ttsUrl, {
+        headers: {
+          "content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(reqBodyJa),
+        method: "POST",
+      })
+    ).json();
+
+    const { audioContent: enAudioContent } = await (
+      await fetch(ttsUrl, {
+        headers: {
+          "content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(reqBodyEn),
+        method: "POST",
+      })
+    ).json();
+
+    const ttsId = crypto.randomUUID();
+
+    const jaTtsCachePathBase = "caches/tts/ja";
+    const jaTtsBuf = Buffer.from(jaAudioContent, "base64");
+    const jaTtsCachePath = `${jaTtsCachePathBase}/${ttsId}${
+      isPremium ? ".wav" : ".mp3"
+    }`;
+
+    const enTtsCachePathBase = "caches/tts/en";
+    const enTtsBuf = Buffer.from(enAudioContent, "base64");
+    const enTtsCachePath = `${enTtsCachePathBase}/${ttsId}${
+      isPremium ? ".wav" : ".mp3"
+    }`;
+
+    await storage.bucket().file(jaTtsCachePath).save(jaTtsBuf);
+    await jaCollection.doc(ttsId).set({
+      ssml: ssmlJa,
+      path: jaTtsCachePath,
+      voice: jaVoiceName,
+      isPremium,
+    });
+
+    await storage.bucket().file(enTtsCachePath).save(enTtsBuf);
+    await enCollection.doc(ttsId).set({
+      ssml: ssmlEn,
+      path: enTtsCachePath,
+      voice: enVoiceName,
+      isPremium,
+    });
+
+    return { jaAudioContent, enAudioContent };
   });
--- src/hooks/useTTS.ts
+++ src/hooks/useTTS.ts
@@ -2,23 +2,21 @@ import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from 'expo-av'
 import * as Crypto from 'expo-crypto'
 import * as FileSystem from 'expo-file-system'
 import { useCallback, useEffect, useRef } from 'react'
-import { GOOGLE_TTS_API_KEY } from 'react-native-dotenv'
+import { DEV_TTS_API_URL, PRODUCTION_TTS_API_URL } from 'react-native-dotenv'
 import { useRecoilValue } from 'recoil'
-import { TTS_CACHE_DIR } from '../constants'
 import speechState from '../store/atoms/speech'
 import stationState from '../store/atoms/station'
-import { currentStationSelector } from '../store/selectors/currentStation'
+import { isDevApp } from '../utils/isDevApp'
+import useAnonymousUser from './useAnonymousUser'
 import useConnectivity from './useConnectivity'
 import { usePrevious } from './usePrevious'
 import { useStoppingState } from './useStoppingState'
-import useTTSCache from './useTTSCache'
 import useTTSText from './useTTSText'
 
 export const useTTS = (): void => {
   const { enabled, losslessEnabled, backgroundEnabled, monetizedPlanEnabled } =
     useRecoilValue(speechState)
   const { selectedBound } = useRecoilValue(stationState)
-  const currentStation = useRecoilValue(currentStationSelector({}))
 
   const firstSpeechRef = useRef(true)
   const playingRef = useRef(false)
@@ -29,8 +27,8 @@ export const useTTS = (): void => {
   const [textJa, textEn] = ttsText
 
   const isInternetAvailable = useConnectivity()
-  const { store, getByText } = useTTSCache()
   const stoppingState = useStoppingState()
+  const user = useAnonymousUser()
 
   const soundJaRef = useRef<Audio.Sound | null>(null)
   const soundEnRef = useRef<Audio.Sound | null>(null)
@@ -78,122 +76,68 @@ export const useTTS = (): void => {
   }, [])
 
   const fetchSpeech = useCallback(
-    async ({
-      textJa,
-      jaId,
-      textEn,
-      enId,
-    }: {
-      textJa: string
-      jaId: string
-      textEn: string
-      enId: string
-    }) => {
+    async ({ textJa, textEn }: { textJa: string; textEn: string }) => {
       if (!textJa.length || !textEn.length) {
         return
       }
 
-      const url = `https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=${GOOGLE_TTS_API_KEY}`
-      const bodyJa = {
-        input: {
-          ssml: `<speak>${textJa.trim()}</speak>`,
-        },
-        voice: {
-          languageCode: 'ja-JP',
-          name:
-            monetizedPlanEnabled && losslessEnabled
-              ? 'ja-JP-Neural2-B'
-              : 'ja-JP-Standard-B',
-        },
-        audioConfig: {
-          audioEncoding:
-            monetizedPlanEnabled && losslessEnabled ? 'LINEAR16' : 'MP3',
-          effectsProfileId: ['handset-class-device'],
+      const reqBody = {
+        data: {
+          ssmlJa: `<speak>${textJa.trim()}</speak>`,
+          ssmlEn: `<speak>${textEn.trim()}</speak>`,
+          premium: monetizedPlanEnabled && losslessEnabled,
         },
       }
 
-      const bodyEn = {
-        input: {
-          ssml: `<speak>${textEn.trim()}</speak>`,
-        },
-        voice: {
-          languageCode: 'en-US',
-          name:
-            monetizedPlanEnabled && losslessEnabled
-              ? 'en-US-Neural2-G'
-              : 'en-US-Standard-G',
-        },
-        audioConfig: {
-          audioEncoding:
-            monetizedPlanEnabled && losslessEnabled ? 'LINEAR16' : 'MP3',
-          effectsProfileId: ['handset-class-device'],
-        },
-      }
+      const idToken = await user?.getIdToken()
 
-      const dataJa = await fetch(url, {
-        headers: {
-          'content-type': 'application/json; charset=UTF-8',
-        },
-        body: JSON.stringify(bodyJa),
-        method: 'POST',
-      })
-      const dataEn = await fetch(url, {
-        headers: {
-          'content-type': 'application/json; charset=UTF-8',
-        },
-        body: JSON.stringify(bodyEn),
-        method: 'POST',
-      })
+      const ttsJson = await (
+        await fetch(isDevApp ? DEV_TTS_API_URL : PRODUCTION_TTS_API_URL, {
+          headers: {
+            'content-type': 'application/json; charset=UTF-8',
+            Authorization: `Bearer ${idToken}`,
+          },
+          body: JSON.stringify(reqBody),
+          method: 'POST',
+        })
+      ).json()
 
-      const baseDir = `${FileSystem.documentDirectory}${TTS_CACHE_DIR}`
-
-      const baseDirInfo = await FileSystem.getInfoAsync(baseDir)
-      if (!baseDirInfo.exists) {
-        await FileSystem.makeDirectoryAsync(baseDir)
-      }
+      const baseDir = FileSystem.cacheDirectory
 
       const extension =
         monetizedPlanEnabled && losslessEnabled ? '.wav' : '.mp3'
 
-      const pathJa = `${baseDir}/${jaId}${extension}`
-      const resJa = await dataJa.json()
-      if (resJa?.audioContent) {
-        await FileSystem.writeAsStringAsync(pathJa, resJa.audioContent, {
-          encoding: FileSystem.EncodingType.Base64,
-        })
+      const pathJa = `${baseDir}/${Crypto.randomUUID()}${extension}`
+      if (ttsJson?.result?.jaAudioContent) {
+        await FileSystem.writeAsStringAsync(
+          pathJa,
+          ttsJson.result.jaAudioContent,
+          {
+            encoding: FileSystem.EncodingType.Base64,
+          }
+        )
       }
-      const pathEn = `${baseDir}/${enId}${extension}`
-      const resEn = await dataEn.json()
-      if (resEn?.audioContent) {
-        await FileSystem.writeAsStringAsync(pathEn, resEn.audioContent, {
-          encoding: FileSystem.EncodingType.Base64,
-        })
+      const pathEn = `${baseDir}/${Crypto.randomUUID()}${extension}`
+      if (ttsJson?.result?.enAudioContent) {
+        await FileSystem.writeAsStringAsync(
+          pathEn,
+          ttsJson.result.enAudioContent,
+          {
+            encoding: FileSystem.EncodingType.Base64,
+          }
+        )
       }
 
       return { pathJa, pathEn }
     },
-    [losslessEnabled, monetizedPlanEnabled]
+    [losslessEnabled, monetizedPlanEnabled, user]
   )
 
   const speech = useCallback(
     async ({ textJa, textEn }: { textJa: string; textEn: string }) => {
-      const cachedPathJa = (await getByText(textJa))?.path
-      const cachedPathEn = (await getByText(textEn))?.path
-
-      // キャッシュにある場合はキャッシュを再生する
-      if (cachedPathJa && cachedPathEn) {
-        await speakFromPath(cachedPathJa, cachedPathEn)
-        return
-      }
-
-      // キャッシュにない場合はGoogle Cloud Text-to-Speech APIを叩く
-      const jaId = Crypto.randomUUID()
-      const enId = Crypto.randomUUID()
       const paths = await fetchSpeech({
         textJa,
-        jaId,
         textEn,
-        enId,
       })
       if (!paths) {
         return
@@ -201,10 +145,8 @@ export const useTTS = (): void => {
       const { pathJa, pathEn } = paths
 
       await speakFromPath(pathJa, pathEn)
-      await store(jaId, textJa, pathJa)
-      await store(enId, textEn, pathEn)
     },
-    [fetchSpeech, getByText, speakFromPath, store]
+    [fetchSpeech, speakFromPath]
   )
 
   useEffect(() => {
