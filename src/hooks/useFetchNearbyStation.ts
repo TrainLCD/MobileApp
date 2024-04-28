03f109e879aec7cd763eaf7e5ff471ad4c173201
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-04-28 14:55:51 +0900

    路線選択のswr化

--- src/hooks/useFetchNearbyStation.ts
+++ src/hooks/useFetchNearbyStation.ts
@@ -1,25 +1,19 @@
-import { LocationObject } from 'expo-location'
 import { useCallback } from 'react'
-import { useSetRecoilState } from 'recoil'
+import useSWRMutation from 'swr/mutation'
 import { GetStationByCoordinatesRequest } from '../../gen/proto/stationapi_pb'
 import { grpcClient } from '../lib/grpc'
-import navigationState from '../store/atoms/navigation'
-import stationState from '../store/atoms/station'
 
-export const useFetchNearbyStation = (): ((
-  location: LocationObject
-) => Promise<void>) => {
-  const setStation = useSetRecoilState(stationState)
-  const setNavigation = useSetRecoilState(navigationState)
-
+export const useFetchNearbyStation = () => {
   const fetchStation = useCallback(
-    async (location: LocationObject | undefined) => {
-      if (!location?.coords) {
+    async (
+      _: string,
+      { arg }: { arg: { latitude: number; longitude: number } | undefined }
+    ) => {
+      if (!arg) {
         return
       }
 
-      const { latitude, longitude } = location.coords
-
+      const { latitude, longitude } = arg
       const req = new GetStationByCoordinatesRequest({
         latitude,
         longitude,
@@ -27,25 +21,15 @@ export const useFetchNearbyStation = (): ((
       })
 
       const data = await grpcClient.getStationsByCoordinates(req)
-
-      if (data) {
-        const { stations } = data
-        setStation((prev) => ({
-          ...prev,
-          station:
-            prev.station?.id !== stations[0]?.id ? stations[0] : prev.station,
-        }))
-        setNavigation((prev) => ({
-          ...prev,
-          stationForHeader:
-            prev.stationForHeader?.id !== stations[0]?.id
-              ? stations[0]
-              : prev.stationForHeader,
-        }))
-      }
+      return data.stations[0]
     },
-    [setNavigation, setStation]
+    []
   )
 
-  return fetchStation
+  const { trigger } = useSWRMutation(
+    '/app.grpc.trainlcd/getStationsByCoordinates',
+    fetchStation
+  )
+
+  return trigger
 }
