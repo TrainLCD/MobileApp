53cde0048d259692ac30ae730741202202b9aecc
Tsubasa SEKIGUCHI <ts@tinykitten.me>
2024-07-03 11:32:44 +0900

    iOS プライバシ画面のlineHeight調整

--- src/screens/Privacy.tsx
+++ src/screens/Privacy.tsx
@@ -33,11 +33,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 12,
     paddingHorizontal: 32,
+    lineHeight: Platform.select({
+      ios: RFValue(18),
+      android: undefined,
+    }),
   },
   headingText: {
     color: '#03a9f4',
     fontSize: RFValue(21),
     fontWeight: 'bold',
+    lineHeight: Platform.select({
+      ios: RFValue(24),
+    }),
   },
   buttons: {
     flexDirection: 'row',
