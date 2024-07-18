255f6562c450f94c382e73963699fe6550cf5e9e
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-07-18 10:32:37 +0900

    種別表示バグ修正

--- src/hooks/useNextTrainType.ts
+++ src/hooks/useNextTrainType.ts
@@ -1,19 +1,28 @@
 import { useMemo } from 'react'
 import { useRecoilValue } from 'recoil'
 import { TrainType } from '../../gen/proto/stationapi_pb'
+import navigationState from '../store/atoms/navigation'
 import stationState from '../store/atoms/station'
 import { currentStationSelector } from '../store/selectors/currentStation'
-import useCurrentTrainType from './useCurrentTrainType'
 import useNextLine from './useNextLine'
 
 const useNextTrainType = (): TrainType | null => {
   const { stations, selectedDirection } = useRecoilValue(stationState)
+  const { trainType } = useRecoilValue(navigationState)
   const nextLine = useNextLine()
   const currentStation = useRecoilValue(currentStationSelector({}))
-  const trainType = useCurrentTrainType()
 
   // 同じ路線でも種別が変わる場合を想定(小田急線等)
   const sameLineNextType = useMemo(() => {
+    if (
+      !(
+        trainType?.line?.id === nextLine?.id &&
+        trainType?.line?.company?.id === nextLine?.company?.id
+      )
+    ) {
+      return
+    }
+
     if (selectedDirection === 'INBOUND') {
       const currentIndex = stations.findIndex(
         (sta) => sta.groupId === currentStation?.groupId
@@ -36,7 +45,16 @@ const useNextTrainType = (): TrainType | null => {
       .map((sta) => sta.trainType)
       .filter((tt) => tt)
       .find((tt) => tt?.typeId !== trainType?.typeId)
-  }, [currentStation?.groupId, selectedDirection, stations, trainType?.typeId])
+  }, [
+    currentStation?.groupId,
+    nextLine?.company?.id,
+    nextLine?.id,
+    selectedDirection,
+    stations,
+    trainType?.line?.company?.id,
+    trainType?.line?.id,
+    trainType?.typeId,
+  ])
 
   const nextTrainType = useMemo(() => {
     return (
