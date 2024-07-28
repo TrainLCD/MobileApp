1b02403daa6059f5bb26fbbc140dc86e57ba0874
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-07-28 21:30:07 +0900

    bump version

--- android/wearable/build.gradle.kts
+++ android/wearable/build.gradle.kts
@@ -12,7 +12,7 @@ android {
       minSdk = 30
       targetSdk = 33
       // 10203011 <- 10203(v1.2.3 version name)+01(build number)+1(Wearable app)
-      versionCode = 80000051
+      versionCode = 80000061
       versionName = "8.0.0"
       vectorDrawables {
           useSupportLibrary = true
