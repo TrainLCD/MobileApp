2ed7a52d0d79c9cc5bac0fdb147a9d8d69cd893e
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-07-28 20:39:05 +0900

    非効率なコードの修正

--- src/hooks/useAnonymousUser.ts
+++ src/hooks/useAnonymousUser.ts
@@ -1,26 +1,26 @@
 import type { FirebaseAuthTypes } from '@react-native-firebase/auth'
 import auth from '@react-native-firebase/auth'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef } from 'react'
 
 /**
  * Recoilが使えない環境の時にもユーザーを持ちたい場合に使います。基本的に `useCachedAnonymousUser` を使ってください。
  */
 const useAnonymousUser = (): FirebaseAuthTypes.User | undefined => {
-  const [user, setUser] = useState<FirebaseAuthTypes.User>()
+  const userRef = useRef<FirebaseAuthTypes.User>()
   useEffect(() => {
     const unsubscribe = auth().onAuthStateChanged((authUser) => {
       if (authUser) {
-        setUser(authUser)
+        userRef.current = authUser
       } else {
         auth()
           .signInAnonymously()
-          .then((credential) => setUser(credential.user))
+          .then((credential) => (userRef.current = credential.user))
       }
     })
     return unsubscribe
-  }, [setUser, user])
+  }, [])
 
-  return user
+  return userRef.current
 }
 
 export default useAnonymousUser
