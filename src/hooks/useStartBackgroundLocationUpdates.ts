87e6971dbc8edea859538d15ca9411fe2a762d32
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-08-02 19:10:22 +0900

    省電力設定機能の実質無効化

--- src/hooks/useStartBackgroundLocationUpdates.ts
+++ src/hooks/useStartBackgroundLocationUpdates.ts
@@ -1,21 +1,19 @@
 import * as Location from 'expo-location'
 import { useEffect } from 'react'
 import { useRecoilValue } from 'recoil'
-import { accuracySelector } from '../store/selectors/accuracy'
 import { autoModeEnabledSelector } from '../store/selectors/autoMode'
 import { translate } from '../translation'
 import { locationTaskName } from '../utils/locationTaskName'
 
 export const useStartBackgroundLocationUpdates = () => {
   const autoModeEnabled = useRecoilValue(autoModeEnabledSelector)
-  const locationServiceAccuracy = useRecoilValue(accuracySelector)
 
   useEffect(() => {
     if (autoModeEnabled) {
       return
     }
     Location.startLocationUpdatesAsync(locationTaskName, {
-      accuracy: locationServiceAccuracy,
+      accuracy: Location.Accuracy.High,
       activityType: Location.ActivityType.OtherNavigation,
       foregroundService: {
         notificationTitle: translate('bgAlertTitle'),
