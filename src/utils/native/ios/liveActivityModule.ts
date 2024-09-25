a69751c8b0668ae439e99e990c52250d630ff8e1
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-09-26 02:58:00 +0900

    iosネイティブ用TSコードのprettier有効化

--- src/utils/native/ios/liveActivityModule.ts
+++ src/utils/native/ios/liveActivityModule.ts
@@ -1,45 +1,42 @@
-import { NativeModules } from 'react-native';
-import { IS_LIVE_ACTIVITIES_ELIGIBLE_PLATFORM } from '../../../constants';
+import { NativeModules } from 'react-native'
+import { IS_LIVE_ACTIVITIES_ELIGIBLE_PLATFORM } from '../../../constants'
 
-
-const { LiveActivityModule } = NativeModules;
-
-
+const { LiveActivityModule } = NativeModules
 
 type LiveActivityWidgetState = {
-  stationName: string;
-  nextStationName: string;
-  stationNumber: string;
-  nextStationNumber: string;
-  approaching: boolean;
-  stopped: boolean;
-  lineName:string
-  lineColor:string
-};
+  stationName: string
+  nextStationName: string
+  stationNumber: string
+  nextStationNumber: string
+  approaching: boolean
+  stopped: boolean
+  lineName: string
+  lineColor: string
+}
 
 export const startLiveActivity = (
   state?: LiveActivityWidgetState
 ): (() => void) | null => {
   if (IS_LIVE_ACTIVITIES_ELIGIBLE_PLATFORM) {
-    return LiveActivityModule?.startLiveActivity?.(state);
+    return LiveActivityModule?.startLiveActivity?.(state)
   }
-  return null;
-};
+  return null
+}
 
 export const updateLiveActivity = (
   state: LiveActivityWidgetState
 ): (() => void) | null => {
   if (IS_LIVE_ACTIVITIES_ELIGIBLE_PLATFORM) {
-    return LiveActivityModule?.updateLiveActivity?.(state);
+    return LiveActivityModule?.updateLiveActivity?.(state)
   }
-  return null;
-};
+  return null
+}
 
 export const stopLiveActivity = (
   state?: LiveActivityWidgetState
 ): (() => void) | null => {
   if (IS_LIVE_ACTIVITIES_ELIGIBLE_PLATFORM) {
-    return LiveActivityModule?.stopLiveActivity?.(state);
+    return LiveActivityModule?.stopLiveActivity?.(state)
   }
-  return null;
-};
+  return null
+}
