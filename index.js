634298800215cf693c1354be62ad2c1142d8622a
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2024-11-28 10:03:33 +0900

    暴走対策処理やっぱり必要だった

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require('fast-text-encoding')
-import { registerRootComponent } from 'expo'
+
 import * as TaskManager from 'expo-task-manager'
-import App from './src'
-import { LOCATION_TASK_NAME } from './src/constants'
-import { setLocation } from './src/hooks/useLocationStore'
 
 TaskManager.unregisterAllTasksAsync().catch(console.error)
+
+let lastTimestamp = 0
 
 if (!TaskManager.isTaskDefined(LOCATION_TASK_NAME)) {
   TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
@@ -13,9 +12,19 @@ if (!TaskManager.isTaskDefined(LOCATION_TASK_NAME)) {
       console.error(error)
       return
     }
-    setLocation(data.locations[0])
+
+    const latestTimestamp = data.locations[0]?.timestamp ?? 0
+    if (lastTimestamp < latestTimestamp) {
+      setLocation(data.locations[0])
+      lastTimestamp = latestTimestamp
+    }
   })
 }
+
+import { registerRootComponent } from 'expo'
+import App from './src'
+import { LOCATION_TASK_NAME } from './src/constants'
+import { setLocation } from './src/hooks/useLocationStore'
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
