71d1009fe92ea7cfee14c5947c705cf9619210d9
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-01-20 00:17:20 +0900

    wip

---
+++ src/constants/autoMode.ts
@@ -0,0 +1,22 @@
+import { LineType } from '../../gen/proto/stationapi_pb';
+
+// 停車秒数
+export const STOPPING_DURATION_IN_SEC = 10 * 1000;
+// 路線種別による最高速度
+export const LINE_TYPE_MAX_SPEEDS_IN_KM_H: Record<LineType, number> = {
+  [LineType.BulletTrain]: 300,
+  [LineType.MonorailOrAGT]: 80,
+  [LineType.Normal]: 90,
+  [LineType.OtherLineType]: 90,
+  [LineType.Subway]: 80,
+  [LineType.Tram]: 40,
+} as const;
+// 路線種別による加速度
+export const LINE_TYPE_MAX_ACCELERATION_IN_KM_H_S: Record<LineType, number> = {
+  [LineType.BulletTrain]: 2.6,
+  [LineType.MonorailOrAGT]: 3.5,
+  [LineType.Normal]: 3.0,
+  [LineType.OtherLineType]: 3.0,
+  [LineType.Subway]: 3.0,
+  [LineType.Tram]: 3.0,
+} as const;
--- src/hooks/useAutoMode.ts
+++ src/hooks/useAutoMode.ts
@@ -1,13 +1,18 @@
-import getCenter from 'geolib/es/getCenter';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import computeDestinationPoint from 'geolib/es/computeDestinationPoint';
+import getDistance from 'geolib/es/getDistance';
+import getRhumbLineBearing from 'geolib/es/getRhumbLineBearing';
+import { useCallback, useMemo, useState } from 'react';
 import { useRecoilValue } from 'recoil';
+import { LineType } from '../../gen/proto/stationapi_pb';
 import {
-  AUTO_MODE_RUNNING_DURATION,
-  AUTO_MODE_WHOLE_DURATION,
-} from '../constants';
+  LINE_TYPE_MAX_ACCELERATION_IN_KM_H_S,
+  LINE_TYPE_MAX_SPEEDS_IN_KM_H,
+} from '../constants/autoMode';
 import lineState from '../store/atoms/line';
 import stationState from '../store/atoms/station';
 import dropEitherJunctionStation from '../utils/dropJunctionStation';
+import { useCurrentLine } from './useCurrentLine';
+import { useInterval } from './useInterval';
 import { useLocationStore } from './useLocationStore';
 import { useLoopLine } from './useLoopLine';
 import useValueRef from './useValueRef';
@@ -18,7 +23,22 @@ const useAutoMode = (enabled: boolean): void => {
     selectedDirection,
     station,
   } = useRecoilValue(stationState);
+  const currentLine = useCurrentLine();
   const { selectedLine } = useRecoilValue(lineState);
+
+  const currentLineType = useMemo(
+    () => currentLine?.lineType ?? LineType.Normal,
+    [currentLine]
+  );
+
+  const maxSpeedInMetersSec = useMemo(
+    () => LINE_TYPE_MAX_SPEEDS_IN_KM_H[currentLineType] / 3.6,
+    [currentLineType]
+  );
+  const maxAccelerationInMS2 = useMemo(
+    () => LINE_TYPE_MAX_ACCELERATION_IN_KM_H_S[currentLineType] / 3.6,
+    [currentLineType]
+  );
 
   const stations = useMemo(
     () => dropEitherJunctionStation(rawStations, selectedDirection),
@@ -33,243 +53,158 @@ const useAutoMode = (enabled: boolean): void => {
   );
   const autoModeInboundIndexRef = useValueRef(autoModeInboundIndex);
   const autoModeOutboundIndexRef = useValueRef(autoModeOutboundIndex);
-  const autoModeApproachingTimerRef = useRef<NodeJS.Timer>();
-  const autoModeArriveTimerRef = useRef<NodeJS.Timer>();
 
   const { isLoopLine } = useLoopLine();
 
-  const startApproachingTimer = useCallback(() => {
-    if (
-      !enabled ||
-      autoModeApproachingTimerRef.current ||
-      !selectedDirection ||
-      !selectedLine
-    ) {
+  const [isDeceleration, setIsDeceleration] = useState(false);
+
+  const [prevTimestamp, setPrevTimestamp] = useState(0);
+  const [wholeDuration, setWholeDuration] = useState(0);
+
+  useInterval(() => {
+    setWholeDuration((prev) => prev + 1);
+    console.log(`${wholeDuration}sec`);
+  }, 1000);
+
+  const step = useCallback(() => {
+    if (!enabled || !selectedDirection || !selectedLine) {
       return;
     }
 
-    const intervalInternal = () => {
-      if (selectedDirection === 'INBOUND') {
-        const index = autoModeInboundIndexRef.current;
+    if (new Date().getTime() - prevTimestamp < 1000) {
+      return;
+    }
 
-        if (!index) {
-          useLocationStore.setState({
-            timestamp: 0,
-            coords: {
-              accuracy: 0,
-              altitude: 0,
-              altitudeAccuracy: -1,
-              speed: 0,
-              heading: 0,
-              latitude: stations[0].latitude,
-              longitude: stations[0].longitude,
-            },
-          });
-          return;
-        }
+    const index = autoModeInboundIndexRef.current;
+    const shouldReset =
+      selectedDirection === 'INBOUND' ? !index : index === stations.length - 1;
 
-        const cur = stations[index];
-        const next = isLoopLine ? stations[index - 1] : stations[index + 1];
+    if (shouldReset) {
+      useLocationStore.setState({
+        timestamp: 0,
+        coords: {
+          accuracy: 0,
+          altitude: 0,
+          altitudeAccuracy: -1,
+          speed: 0,
+          heading: 0,
+          latitude: stations[0].latitude,
+          longitude: stations[0].longitude,
+        },
+      });
+      return;
+    }
 
-        if (cur && next) {
-          const center = getCenter([
-            {
-              latitude: cur.latitude,
-              longitude: cur.longitude,
-            },
-            {
-              latitude: next.latitude,
-              longitude: next.longitude,
-            },
-          ]);
+    const cur = stations[index];
+    const next =
+      selectedDirection === 'INBOUND'
+        ? isLoopLine
+          ? stations[index - 1]
+          : stations[index + 1]
+        : isLoopLine
+          ? stations[index + 1]
+          : stations[index - 1];
 
-          if (center) {
-            useLocationStore.setState({
-              timestamp: 0,
-              coords: {
-                ...center,
-                accuracy: 0,
-                altitude: 0,
-                altitudeAccuracy: -1,
-                speed: 0,
-                heading: 0,
-              },
-            });
+    if (cur && next) {
+      useLocationStore.setState((prev) => {
+        const currentDistanceForNextStation = getDistance(
+          {
+            lat: prev?.coords?.latitude ?? 0,
+            lon: prev?.coords?.longitude ?? 0,
+          },
+          {
+            lat: next?.latitude ?? 0,
+            lon: next?.longitude ?? 0,
           }
-        }
-      } else {
-        const index = autoModeOutboundIndexRef.current;
+        );
 
-        if (index === stations.length - 1) {
-          useLocationStore.setState({
-            timestamp: 0,
-            coords: {
-              accuracy: 0,
-              altitude: 0,
-              altitudeAccuracy: -1,
-              speed: 0,
-              heading: 0,
-              latitude: stations[stations.length - 1].latitude,
-              longitude: stations[stations.length - 1].longitude,
-            },
-          });
-          return;
-        }
+        const speedInMh = (() => {
+          const prevSpeed = prev?.coords.speed ?? 0;
 
-        const cur = stations[index];
-        const next = isLoopLine ? stations[index + 1] : stations[index - 1];
+          const isAcceleration =
+            (prev?.coords.speed ?? 0) < maxSpeedInMetersSec;
 
-        if (cur && next) {
-          const center = getCenter([
-            {
-              latitude: cur.latitude,
-              longitude: cur.longitude,
-            },
-            {
-              latitude: next.latitude,
-              longitude: next.longitude,
-            },
-          ]);
-
-          if (center) {
-            useLocationStore.setState({
-              timestamp: 0,
-              coords: {
-                ...center,
-                accuracy: 0,
-                altitude: 0,
-                altitudeAccuracy: -1,
-                speed: 0,
-                heading: 0,
-              },
-            });
+          if (Math.floor(prevSpeed) > 0) {
+            const isStoppable =
+              Math.floor(currentDistanceForNextStation / prevSpeed) > 0;
+            if (!isStoppable) {
+              setIsDeceleration(true);
+            }
           }
-        }
-      }
-    };
 
-    intervalInternal();
+          if (isDeceleration) {
+            const nextSpeed = prevSpeed - maxAccelerationInMS2;
+            if (nextSpeed < 0) {
+              return 0;
+            }
+            return nextSpeed;
+          }
 
-    const interval = setInterval(intervalInternal, AUTO_MODE_RUNNING_DURATION);
+          if (isAcceleration) {
+            return prevSpeed + maxAccelerationInMS2;
+          }
 
-    autoModeApproachingTimerRef.current = interval;
+          return maxSpeedInMetersSec;
+        })();
+
+        console.log(
+          `for ${next.nameRoman} Station`,
+          currentDistanceForNextStation,
+          'm',
+          'speed',
+          speedInMh,
+          'm/s'
+        );
+
+        const nextBearing = getRhumbLineBearing(
+          {
+            latitude: prev?.coords.latitude ?? 0,
+            longitude: prev?.coords.longitude ?? 0,
+          },
+          {
+            latitude: next?.latitude ?? 0,
+            longitude: next?.longitude ?? 0,
+          }
+        );
+
+        const nextPoint = computeDestinationPoint(
+          {
+            lat: prev?.coords.latitude ?? 0,
+            lon: prev?.coords.longitude ?? 0,
+          },
+          speedInMh,
+          nextBearing
+        );
+
+        setPrevTimestamp(new Date().getTime());
+
+        return {
+          timestamp: 0,
+          coords: {
+            ...nextPoint,
+            accuracy: 0,
+            altitude: 0,
+            altitudeAccuracy: -1,
+            speed: speedInMh,
+            heading: 0,
+          },
+        };
+      });
+    }
   }, [
-    autoModeInboundIndexRef,
-    autoModeOutboundIndexRef,
+    autoModeInboundIndexRef.current,
     enabled,
     isLoopLine,
     selectedDirection,
     selectedLine,
     stations,
+    prevTimestamp,
+    maxAccelerationInMS2,
+    maxSpeedInMetersSec,
+    isDeceleration,
   ]);
 
-  useEffect(() => {
-    startApproachingTimer();
-  }, [startApproachingTimer]);
-
-  const startArriveTimer = useCallback(() => {
-    const direction = selectedDirection;
-
-    if (
-      !enabled ||
-      autoModeArriveTimerRef.current ||
-      !direction ||
-      !selectedLine
-    ) {
-      return;
-    }
-
-    const intervalInternal = () => {
-      if (direction === 'INBOUND') {
-        const index = autoModeInboundIndexRef.current;
-
-        const next = stations[index];
-
-        if (!isLoopLine && index === stations.length - 1) {
-          setAutoModeInboundIndex(0);
-        } else {
-          setAutoModeInboundIndex((prev) => (isLoopLine ? prev - 1 : prev + 1));
-        }
-
-        if (!index && isLoopLine) {
-          setAutoModeInboundIndex(stations.length - 1);
-        }
-
-        if (next) {
-          useLocationStore.setState({
-            timestamp: 0,
-            coords: {
-              latitude: next.latitude,
-              longitude: next.longitude,
-              accuracy: 0,
-              altitude: 0,
-              altitudeAccuracy: -1,
-              speed: 0,
-              heading: 0,
-            },
-          });
-        }
-      } else if (direction === 'OUTBOUND') {
-        const index = autoModeOutboundIndexRef.current;
-
-        const next = stations[index];
-        if (!isLoopLine && !index) {
-          setAutoModeOutboundIndex(stations.length);
-        } else {
-          setAutoModeOutboundIndex((prev) =>
-            isLoopLine ? prev + 1 : prev - 1
-          );
-        }
-
-        if (index === stations.length - 1 && isLoopLine) {
-          setAutoModeOutboundIndex(0);
-        }
-
-        if (next) {
-          useLocationStore.setState({
-            timestamp: 0,
-            coords: {
-              latitude: next.latitude,
-              longitude: next.longitude,
-              accuracy: 0,
-              altitude: 0,
-              altitudeAccuracy: -1,
-              speed: 0,
-              heading: 0,
-            },
-          });
-        }
-      }
-    };
-
-    intervalInternal();
-
-    const interval = setInterval(intervalInternal, AUTO_MODE_WHOLE_DURATION);
-    autoModeArriveTimerRef.current = interval;
-  }, [
-    autoModeInboundIndexRef,
-    autoModeOutboundIndexRef,
-    enabled,
-    isLoopLine,
-    selectedDirection,
-    selectedLine,
-    stations,
-  ]);
-
-  useEffect(() => {
-    startArriveTimer();
-  }, [startArriveTimer]);
-
-  useEffect(() => {
-    return () => {
-      if (autoModeApproachingTimerRef.current) {
-        clearInterval(autoModeApproachingTimerRef.current);
-      }
-      if (autoModeArriveTimerRef.current) {
-        clearInterval(autoModeArriveTimerRef.current);
-      }
-    };
-  }, []);
+  useInterval(step, 1000);
 };
 
 export default useAutoMode;
