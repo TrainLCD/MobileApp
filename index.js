d013c7d442b9447bd0b11ad8cbb26c7b2e4e9e1e
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-01-27 06:13:09 +0900

    Sentryの環境分け

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ require('fast-text-encoding');
 import * as Sentry from '@sentry/react-native';
 import { registerRootComponent } from 'expo';
 import * as TaskManager from 'expo-task-manager';
-import { SENTRY_DSN } from 'react-native-dotenv';
+import { DEV_SENTRY_DSN, PRODUCTION_SENTRY_DSN } from 'react-native-dotenv';
 import App from './src';
 import { LOCATION_TASK_NAME } from './src/constants';
 import { setLocation } from './src/hooks/useLocationStore';
+import { isDevApp } from './src/utils/isDevApp';
 
 if (!__DEV__) {
   Sentry.init({
-    dsn: SENTRY_DSN,
+    dsn: isDevApp ? DEV_SENTRY_DSN : PRODUCTION_SENTRY_DSN,
     enableAutoSessionTracking: true,
     tracesSampleRate: 1.0,
     profilesSampleRate: 1.0,
