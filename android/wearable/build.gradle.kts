7e1d711bcc1d5507435534880f877119932e317d
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-02-14 20:18:09 +0900

    bump version

--- android/wearable/build.gradle.kts
+++ android/wearable/build.gradle.kts
@@ -38,7 +38,7 @@ android {
       applicationIdSuffix = ".dev"
       versionNameSuffix = "-dev"
       // 10203011 <- 10203(v1.2.3 version name)+01(build number)+1(Wearable app)
-      versionCode = 80500161
+      versionCode = 80500171
       versionName = "8.5.0"
     }
     create("prod") {
