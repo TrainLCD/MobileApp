45d674670c49de1d6d50f6e2ddd75526c3682b72
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-02-28 07:00:39 +0900

    山手乗り換えパネルタップ乗換対応

--- src/components/TransfersYamanote.tsx
+++ src/components/TransfersYamanote.tsx
@@ -1,8 +1,13 @@
 import React, { useMemo } from 'react';
-import { Dimensions, ScrollView, StyleSheet, View } from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import {
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import type { Station } from '../../gen/proto/stationapi_pb';
+import { type Line, Station } from '../../gen/proto/stationapi_pb';
 import { NUMBERING_ICON_SIZE, parenthesisRegexp } from '../constants';
 import useGetLineMark from '../hooks/useGetLineMark';
 import useTransferLines from '../hooks/useTransferLines';
@@ -14,11 +19,12 @@ import TransferLineMark from './TransferLineMark';
 import Typography from './Typography';
 
 interface Props {
-  onPress: () => void;
+  onPress: (station?: Station) => void;
   station: Station;
 }
 
 const styles = StyleSheet.create({
+  container: { flex: 1 },
   transferLine: {
     flex: isTablet ? 0 : 1,
     marginBottom: isTablet ? 32 : 8,
@@ -33,12 +39,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   transferList: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
     paddingTop: isTablet ? 32 : 24,
     padding: 24,
+    alignSelf: 'center',
+    alignItems: 'center',
   },
   transferLineInner: {
     flexDirection: 'row',
@@ -67,61 +71,71 @@ const TransfersYamanote: React.FC<Props> = ({ onPress, station }: Props) => {
 
   const flexBasis = useMemo(() => Dimensions.get('screen').width / 3, []);
 
-  const renderTransferLines = (): (JSX.Element | null)[] =>
-    lines.map((line) => {
-      if (!station) {
-        return null;
-      }
-      const lineMark = getLineMarkFunc({ line });
+  const renderTransferLine = ({
+    item: line,
+  }: { item: Line; index: number }) => {
+    if (!station) {
+      return null;
+    }
+    const lineMark = getLineMarkFunc({ line });
 
-      return (
-        <View
-          style={[
-            styles.transferLine,
-            {
-              marginLeft: safeAreaLeft,
-              marginRight: safeAreaRight,
-              flexBasis,
-            },
-          ]}
-          key={line.id}
-        >
-          <View style={styles.transferLineInner}>
-            {lineMark ? (
-              <TransferLineMark
-                line={line}
-                mark={lineMark}
-                size={NUMBERING_ICON_SIZE.MEDIUM}
-              />
-            ) : (
-              <TransferLineDot line={line} />
-            )}
+    return (
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => onPress(new Station({ ...line.station, line }))}
+        style={[
+          styles.transferLine,
+          {
+            marginLeft: safeAreaLeft,
+            marginRight: safeAreaRight,
+            flexBasis,
+          },
+        ]}
+        key={line.id}
+      >
+        <View style={styles.transferLineInner}>
+          {lineMark ? (
+            <TransferLineMark
+              line={line}
+              mark={lineMark}
+              size={NUMBERING_ICON_SIZE.MEDIUM}
+            />
+          ) : (
+            <TransferLineDot line={line} />
+          )}
 
-            <View style={styles.lineNameContainer}>
-              <Typography style={styles.lineName}>
-                {line.nameShort.replace(parenthesisRegexp, '')}
-              </Typography>
-              <Typography style={styles.lineNameEn}>
-                {line.nameRoman?.replace(parenthesisRegexp, '')}
-              </Typography>
-            </View>
+          <View style={styles.lineNameContainer}>
+            <Typography style={styles.lineName}>
+              {line.nameShort.replace(parenthesisRegexp, '')}
+            </Typography>
+            <Typography style={styles.lineNameEn}>
+              {line.nameRoman?.replace(parenthesisRegexp, '')}
+            </Typography>
           </View>
         </View>
-      );
-    });
+      </TouchableOpacity>
+    );
+  };
 
   return (
-    <ScrollView>
-      <TouchableWithoutFeedback onPress={onPress}>
-        <View style={styles.header}>
-          <Typography style={styles.headerText}>
-            {translate('transferYamanote')}
-          </Typography>
-        </View>
-
-        <View style={styles.transferList}>{renderTransferLines()}</View>
-      </TouchableWithoutFeedback>
-    </ScrollView>
+    <TouchableOpacity
+      style={styles.container}
+      activeOpacity={1}
+      onPress={() => onPress()}
+    >
+      <View style={styles.header}>
+        <Typography style={styles.headerText}>
+          {translate('transferYamanote')}
+        </Typography>
+      </View>
+      <FlatList
+        data={lines}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={renderTransferLine}
+        contentContainerStyle={styles.transferList}
+        numColumns={2}
+      />
+    </TouchableOpacity>
   );
 };
 
