2eacf79144061231ee76f3e936e7a02c7a9829cf
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-03-04 07:34:07 +0900

    列車種別が直通後変更されないバグを修正

--- src/hooks/useCurrentTrainType.ts
+++ src/hooks/useCurrentTrainType.ts
@@ -2,20 +2,21 @@ import { useMemo } from 'react';
 import { useRecoilValue } from 'recoil';
 import { type TrainType, TrainTypeKind } from '../../gen/proto/stationapi_pb';
 import navigationState from '../store/atoms/navigation';
-import { useCurrentLine } from './useCurrentLine';
+import { useCurrentStation } from './useCurrentStation';
 
 const useCurrentTrainType = (): TrainType | null => {
   const { trainType, fromBuilder } = useRecoilValue(navigationState);
-  const currentLine = useCurrentLine();
+
+  const currentStation = useCurrentStation();
 
   const currentTrainType = useMemo(
     () =>
       ((trainType?.lines?.length ?? 1) > 1 ||
         trainType?.kind === TrainTypeKind.Branch) &&
       !fromBuilder
-        ? trainType?.lines?.find((l) => l.id === currentLine?.id)?.trainType
+        ? currentStation?.trainType
         : (trainType ?? null),
-    [currentLine?.id, fromBuilder, trainType]
+    [currentStation, fromBuilder, trainType]
   );
 
   return currentTrainType ?? null;
