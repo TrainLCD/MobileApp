be5374eacc668136d424080d93fcc0b241b658f8
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-03-04 08:13:08 +0900

    通過駅を通過中種別が勝手に消えないように修正

--- src/hooks/useCurrentTrainType.ts
+++ src/hooks/useCurrentTrainType.ts
@@ -1,25 +1,37 @@
-import { useMemo } from 'react';
+import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { type TrainType, TrainTypeKind } from '../../gen/proto/stationapi_pb';
 import navigationState from '../store/atoms/navigation';
 import { useCurrentStation } from './useCurrentStation';
+import getIsPass from '../utils/isPass';
 
 const useCurrentTrainType = (): TrainType | null => {
   const { trainType, fromBuilder } = useRecoilValue(navigationState);
 
   const currentStation = useCurrentStation();
 
-  const currentTrainType = useMemo(
-    () =>
-      ((trainType?.lines?.length ?? 1) > 1 ||
-        trainType?.kind === TrainTypeKind.Branch) &&
-      !fromBuilder
-        ? currentStation?.trainType
-        : (trainType ?? null),
-    [currentStation, fromBuilder, trainType]
+  const [cachedTrainType, setCachedTrainType] = useState(
+    currentStation?.trainType ?? trainType
   );
 
-  return currentTrainType ?? null;
+  useEffect(() => {
+    if (!trainType) {
+      setCachedTrainType(null);
+    }
+  }, [trainType]);
+
+  useEffect(() => {
+    if (
+      ((trainType?.lines?.length ?? 1) > 1 ||
+        trainType?.kind === TrainTypeKind.Branch) &&
+      !fromBuilder &&
+      !getIsPass(currentStation)
+    ) {
+      setCachedTrainType(currentStation?.trainType ?? null);
+    }
+  }, [currentStation, fromBuilder, trainType]);
+
+  return cachedTrainType;
 };
 
 export default useCurrentTrainType;
