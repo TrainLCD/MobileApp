5f1d54760309bc6a05d61df952574e586d9c4dff
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-03-11 08:45:06 +0900

    位置情報取得コードの改良

--- src/hooks/useStartBackgroundLocationUpdates.ts
+++ src/hooks/useStartBackgroundLocationUpdates.ts
@@ -8,48 +8,28 @@ import { setLocation } from './useLocationStore';
 
 export const useStartBackgroundLocationUpdates = () => {
   const bgPermGranted = useLocationPermissionsGranted();
+  const { autoModeEnabled } = useApplicationFlagStore();
 
   useEffect(() => {
-    const autoModeEnabled = useApplicationFlagStore.getState()?.autoModeEnabled;
-    if (autoModeEnabled) {
-      return;
-    }
-
-    let watchPositionSub: Location.LocationSubscription | null = null;
-
     (async () => {
-      try {
-        if (bgPermGranted) {
-          await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-            // NOTE: BestForNavigationにしたら暴走時のCPU使用率が50%ほど低下した
-            accuracy: Location.Accuracy.BestForNavigation,
-            // NOTE: マップマッチが勝手に行われると電車での経路と大きく異なることがあるはずなので
-            // OtherNavigationは必須
-            activityType: Location.ActivityType.OtherNavigation,
-            timeInterval: 5000,
-            distanceInterval: 10,
-            foregroundService: {
-              notificationTitle: translate('bgAlertTitle'),
-              notificationBody: translate('bgAlertContent'),
-              killServiceOnDestroy: true,
-            },
-          });
-        }
-      } catch (err) {
-        console.error(err);
+      if (autoModeEnabled || !bgPermGranted) {
+        return;
       }
-
       try {
-        if (!bgPermGranted) {
-          watchPositionSub = await Location.watchPositionAsync(
-            {
-              accuracy: Location.Accuracy.BestForNavigation,
-              timeInterval: 5000,
-              distanceInterval: 10,
-            },
-            setLocation
-          );
-        }
+        await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+          // NOTE: BestForNavigationにしたら暴走時のCPU使用率が50%ほど低下した
+          accuracy: Location.Accuracy.BestForNavigation,
+          // NOTE: マップマッチが勝手に行われると電車での経路と大きく異なることがあるはずなので
+          // OtherNavigationは必須
+          activityType: Location.ActivityType.OtherNavigation,
+          timeInterval: 5000,
+          distanceInterval: 10,
+          foregroundService: {
+            notificationTitle: translate('bgAlertTitle'),
+            notificationBody: translate('bgAlertContent'),
+            killServiceOnDestroy: true,
+          },
+        });
       } catch (err) {
         console.error(err);
       }
@@ -57,7 +37,33 @@ export const useStartBackgroundLocationUpdates = () => {
 
     return () => {
       Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+    };
+  }, [autoModeEnabled, bgPermGranted]);
+
+  useEffect(() => {
+    let watchPositionSub: Location.LocationSubscription | null = null;
+
+    (async () => {
+      if (autoModeEnabled || bgPermGranted) {
+        return;
+      }
+
+      try {
+        watchPositionSub = await Location.watchPositionAsync(
+          {
+            accuracy: Location.Accuracy.BestForNavigation,
+            timeInterval: 5000,
+            distanceInterval: 10,
+          },
+          setLocation
+        );
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+
+    return () => {
       watchPositionSub?.remove();
     };
-  }, [bgPermGranted]);
+  }, [autoModeEnabled, bgPermGranted]);
 };
