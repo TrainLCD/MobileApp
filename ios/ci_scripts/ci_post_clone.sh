ffe05bbc4cd7cc83c8f4ae3418e11718d37f154c
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-05-18 11:41:33 +0900

    package.jsonから除いたパッケージを npm i -g

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 brew install cocoapods node ccache
-npm install -g npm
+npm install -g npm @connectrpc/protoc-gen-connect-query@2 @connectrpc/protoc-gen-connect-es@1
 npm ci
 pod install
