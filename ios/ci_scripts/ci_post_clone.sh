7693989c6b48ebd16565fd5a092a90919fdb1281
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-06-01 23:40:49 +0900

    モバイルアプリ pnpm移行

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 brew install cocoapods node ccache
-npm install -g npm @connectrpc/protoc-gen-connect-query@1 @connectrpc/protoc-gen-connect-es@1
-npm ci --legacy-peer-deps # TODO: --legacy-peer-depsをしばく
+npm install -g npm @connectrpc/protoc-gen-connect-query@1 @connectrpc/protoc-gen-connect-es@1 pnpm@10
+pnpm i
 pod install
