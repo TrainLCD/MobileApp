4ae29bc656970a5e00dd7624a1c8bf1e76c14936
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-06-02 08:51:53 +0900

    ☺️Xcode Cloud --shamefully-hoistを外してみる☺️

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 brew install cocoapods node ccache
 npm install -g npm @connectrpc/protoc-gen-connect-query@1 @connectrpc/protoc-gen-connect-es@1 pnpm@10
-pnpm i --shamefully-hoist
+pnpm i
 pod install
