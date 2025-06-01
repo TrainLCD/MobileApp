9786f4af738625da82db8abf266eac6d50eff36e
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-06-02 08:26:07 +0900

    🫣Xcode Cloud --shamefully-hoist🫣

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 brew install cocoapods node ccache
 npm install -g npm @connectrpc/protoc-gen-connect-query@1 @connectrpc/protoc-gen-connect-es@1 pnpm@10
-pnpm i
+pnpm i --shamefully-hoist
 pod install
