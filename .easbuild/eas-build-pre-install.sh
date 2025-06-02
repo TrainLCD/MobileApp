b7311e39b9635160802a2db5ced7a3c9fc8fe04c
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-06-02 22:32:24 +0900

    多少安全なシェルスクリプトに

--- .easbuild/eas-build-pre-install.sh
+++ .easbuild/eas-build-pre-install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+bash +set -euo pipefail +
+
 
 echo $DOTENV_LOCAL | base64 --decode > .env.local
 
 if [[ "$EAS_BUILD_PLATFORM" == "android" ]]; then
-  echo $DEV_GOOGLE_SERVICES_JSON | base64 --decode > android/app/src/dev/google-services.json
-  echo $PROD_GOOGLE_SERVICES_JSON | base64 --decode > android/app/src/prod/google-services.json
-  echo $SENTRY_PROPERTIES | base64 --decode > android/sentry.properties
+  echo "$DEV_GOOGLE_SERVICES_JSON" | base64 --decode > android/app/src/dev/google-services.json
+  echo "$PROD_GOOGLE_SERVICES_JSON" | base64 --decode > android/app/src/prod/google-services.json
+  echo "$SENTRY_PROPERTIES" | base64 --decode > android/sentry.properties
 elif [[ "$EAS_BUILD_PLATFORM" == "ios" ]]; then
-  echo $PROD_GOOGLE_SERVICE_INFO_PLIST | base64 --decode > ios/Schemes/Prod/GoogleService-Info.plist
-  echo $DEV_GOOGLE_SERVICE_INFO_PLIST | base64 --decode > ios/Schemes/Dev/GoogleService-Info.plist
-  echo $SENTRY_PROPERTIES | base64 --decode > ios/sentry.properties
+  echo "$PROD_GOOGLE_SERVICE_INFO_PLIST" | base64 --decode > ios/Schemes/Prod/GoogleService-Info.plist
+  echo "$DEV_GOOGLE_SERVICE_INFO_PLIST" | base64 --decode > ios/Schemes/Dev/GoogleService-Info.plist
+  echo "$SENTRY_PROPERTIES" | base64 --decode > ios/sentry.properties
 fi
