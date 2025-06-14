aaa9859ad7a90102648d22bbe641185e3953b003
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-06-14 19:20:19 +0900

    v8.13.3🎉

--- android/wearable/build.gradle.kts
+++ android/wearable/build.gradle.kts
@@ -39,13 +39,13 @@ android {
       applicationIdSuffix = ".dev"
       versionNameSuffix = "-dev"
       // 10203011 <- 10203(v1.2.3 version name)+01(build number)+1(Wearable app)
-      versionCode = 90000161
-      versionName = "9.0.0"
+      versionCode = 81303001
+      versionName = "8.13.3"
     }
     create("prod") {
       dimension = "environment"
-      versionCode = 90000000
-      versionName = "9.0.0"
+      versionCode = 81303001
+      versionName = "8.13.3"
     }
   }
 
