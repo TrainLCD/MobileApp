a3e8fc13e2afcec83dd54f4f6eff9d9d03af4707
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-06-22 08:17:06 +0900

    オートモードが有効になった時明示的に位置情報取得を停止する

--- src/hooks/useSimulationMode.ts
+++ src/hooks/useSimulationMode.ts
@@ -1,3 +1,4 @@
+import * as Location from 'expo-location';
 import computeDestinationPoint from 'geolib/es/computeDestinationPoint';
 import getGreatCircleBearing from 'geolib/es/getGreatCircleBearing';
 import getPathLength from 'geolib/es/getPathLength';
@@ -8,6 +9,7 @@ import {
   LINE_TYPE_MAX_ACCEL_IN_M_S,
   LINE_TYPE_MAX_DECEL_IN_M_S,
   LINE_TYPE_MAX_SPEEDS_IN_M_S,
+  LOCATION_TASK_NAME,
   TRAIN_TYPE_KIND_MAX_SPEEDS_IN_M_S,
 } from '~/constants';
 import { LineType } from '~/gen/proto/stationapi_pb';
@@ -73,6 +75,21 @@ export const useSimulationMode = (): void => {
   const enabled = useMemo(() => {
     return !enableLegacyAutoMode && autoModeEnabled;
   }, [enableLegacyAutoMode, autoModeEnabled]);
+
+  useEffect(() => {
+    const stopLocationUpdatesAsync = async () => {
+      try {
+        const hasStartedLocationUpdates =
+          await Location.hasStartedLocationUpdatesAsync(LOCATION_TASK_NAME);
+        if (hasStartedLocationUpdates) {
+          await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    stopLocationUpdatesAsync();
+  }, []);
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: プロファイル生成は初回のみ
   useEffect(() => {
