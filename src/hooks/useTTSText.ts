6a908b478b27ea2fb982492ab416582e8bbd3549
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-07-12 10:43:26 +0900

    TTSの駅番号が実際と異なるバグを修正

--- src/hooks/useTTSText.ts
+++ src/hooks/useTTSText.ts
@@ -16,11 +16,11 @@ import { useIsTerminus } from './useIsTerminus';
 import { useLoopLine } from './useLoopLine';
 import { useLoopLineBound } from './useLoopLineBound';
 import { useNextStation } from './useNextStation';
-import { useNumbering } from './useNumbering';
 import { useSlicedStations } from './useSlicedStations';
 import { useStoppingState } from './useStoppingState';
 import { useThemeStore } from './useThemeStore';
 import { useTransferLines } from './useTransferLines';
+import { useStationNumberIndexFunc } from './useStationNumberIndexFunc';
 
 const EMPTY_TTS_TEXT = {
   [APP_THEME.TOKYO_METRO]: { NEXT: '', ARRIVING: '' },
@@ -46,7 +46,6 @@ export const useTTSText = (
 
   const connectedLinesOrigin = useConnectedLines();
   const transferLinesOriginal = useTransferLines();
-  const [nextStationNumber] = useNumbering(false);
   const currentTrainTypeOrigin = useCurrentTrainType();
   const loopLineBoundJa = useLoopLineBound(false);
   const loopLineBoundEn = useLoopLineBound(false, 'EN');
@@ -56,6 +55,16 @@ export const useTTSText = (
   const { isLoopLine, isPartiallyLoopLine } = useLoopLine();
   const slicedStationsOrigin = useSlicedStations();
   const stoppingState = useStoppingState();
+  const getStationNumberIndex = useStationNumberIndexFunc();
+
+  const nextStationNumber = useMemo(() => {
+    if (!nextStationOrigin) {
+      return;
+    }
+
+    const stationNumberIndex = getStationNumberIndex(nextStationOrigin);
+    return nextStationOrigin?.stationNumbers[stationNumberIndex];
+  }, [getStationNumberIndex, nextStationOrigin]);
 
   const replaceJapaneseText = useCallback(
     (name: string | undefined, nameKatakana: string | undefined) =>
