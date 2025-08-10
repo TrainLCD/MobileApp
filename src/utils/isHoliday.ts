843ddc227b6fffe621398c1c06e5e5cb70200c36
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-08-10 11:41:47 +0900

    ライブラリで祝日判定

--- src/utils/isHoliday.ts
+++ src/utils/isHoliday.ts
@@ -1,6 +1,6 @@
+import holiday_jp from '@holiday-jp/holiday_jp';
 import dayjs from 'dayjs';
 import memoize from 'lodash/memoize';
-import { JAPANESE_NATIONAL_HOLIDAYS } from '~/constants';
 
 const formatDate = (date: Date) =>
   [date.getFullYear(), date.getMonth(), date.getDate()].join('-');
@@ -8,10 +8,7 @@ const formatDate = (date: Date) =>
 export const getIsHoliday = memoize((date: Date): boolean => {
   const now = dayjs(date);
 
-  const isNationalHoliday = JAPANESE_NATIONAL_HOLIDAYS.some((ev) => {
-    const eventDay = dayjs(ev.date);
-    return now.isSame(eventDay, 'month') && now.isSame(eventDay, 'date');
-  });
+  const isNationalHoliday = holiday_jp.isHoliday(date);
   const isWeekend = now.day() === 0 || now.day() === 6;
 
   return isWeekend || isNationalHoliday;
