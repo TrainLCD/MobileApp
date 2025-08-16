998e8735405eb8a7388faa352ef2105482ae35ea
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-08-16 20:10:01 +0900

    metro.config.js修正

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,6 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
-const {
-  withSentryConfig
-} = require('@sentry/react-native/metro');
+const { getDefaultConfig } = require('expo/metro-config');
+const { withSentryConfig } = require('@sentry/react-native/metro');
 
-const config = getDefaultConfig(__dirname);
-module.exports = withSentryConfig(config);
+const baseConfig = getDefaultConfig(__dirname);
+const useSentry = process.env.NODE_ENV === 'production';
+module.exports = useSentry ? withSentryConfig(baseConfig) : baseConfig;
