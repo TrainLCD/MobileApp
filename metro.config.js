1628735ca5cef499b2356ef728f2f671f8de40e2
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-08-16 19:58:47 +0900

    quote統一

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 const { getDefaultConfig } = require('@react-native/metro-config');
 const {
   withSentryConfig
-} = require("@sentry/react-native/metro");
+} = require('@sentry/react-native/metro');
 
 const config = getDefaultConfig(__dirname);
 module.exports = withSentryConfig(config);
