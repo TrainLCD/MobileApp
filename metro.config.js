16cb01c3b02e7cb6fa1373dd46e9e8306d9c28e3
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-10-19 22:22:02 +0900

    Add .d.ts to source extensions in metro.config.js and update pnpm-lock.yaml to include rxjs dependency

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withSentryConfig } = require('@sentry/react-native/metro');
 
 const baseConfig = getDefaultConfig(__dirname);
+
+// Add .d.ts to source extensions
+baseConfig.resolver.sourceExts.push('d.ts');
+
 const useSentry = process.env.NODE_ENV === 'production';
 module.exports = useSentry ? withSentryConfig(baseConfig) : baseConfig;
