bbe184180add39c66e81a240b5d4f374f3f24318
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-10-27 23:55:18 +0900

    useScaleのメモ化

--- src/hooks/useScale.ts
+++ src/hooks/useScale.ts
@@ -1,4 +1,5 @@
-import { useWindowDimensions } from 'react-native';
+import { useCallback } from 'react';
+import { PixelRatio, useWindowDimensions } from 'react-native';
 
 const standardWidth = 375.0;
 const standardHeight = 667.0;
@@ -6,10 +7,16 @@ const standardHeight = 667.0;
 export const useScale = () => {
   const { width: myWidth, height: myHeight } = useWindowDimensions();
 
-  const widthScale = (dimension: number): number =>
-    (dimension / standardWidth) * myWidth;
-  const heightScale = (dimension: number): number =>
-    (dimension / standardHeight) * myHeight;
+  const widthScale = useCallback(
+    (dimension: number): number =>
+      PixelRatio.roundToNearestPixel((dimension / standardWidth) * myWidth),
+    [myWidth]
+  );
+  const heightScale = useCallback(
+    (dimension: number): number =>
+      PixelRatio.roundToNearestPixel((dimension / standardHeight) * myHeight),
+    [myHeight]
+  );
 
   return { widthScale, heightScale, myWidth, myHeight };
 };
