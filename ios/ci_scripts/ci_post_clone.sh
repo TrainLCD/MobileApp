6db577551e26f693e382f2351c5267836b7f00b5
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-10-31 08:33:21 +0900

    brewでnodeを入れる

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,9 +2,7 @@
 
 set -euo pipefail
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
-nvm install 24
+brew install node@24
 npm install -g pnpm@10
 pnpm approve-builds
 pnpm i
