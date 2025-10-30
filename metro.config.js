124813b603ed1a02e642e8919135aa1f6de1bb53
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-10-30 23:59:17 +0900

    そもそも起動しなくなってしまったバグを修正

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,7 @@
-const { getDefaultConfig } = require("@react-native/metro-config");
-const { withSentryConfig } = require("@sentry/react-native/metro");
-const config = getDefaultConfig(__dirname);
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+const config = getSentryExpoConfig(__dirname);
 
 // Add .d.ts to source extensions
 config.resolver.sourceExts.push('d.ts');
 
-module.exports = withSentryConfig(config);
+module.exports = config;
