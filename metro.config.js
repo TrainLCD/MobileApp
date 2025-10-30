08ce574228ca503092889ddc51f3042d46673e94
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-10-30 15:47:53 +0900

    getSentryExpoConfig

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,8 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withSentryConfig } = require('@sentry/react-native/metro');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 
-const baseConfig = getDefaultConfig(__dirname);
+const baseConfig = getSentryExpoConfig(__dirname);
 
 // Add .d.ts to source extensions
 baseConfig.resolver.sourceExts.push('d.ts');
 
-const useSentry = process.env.NODE_ENV === 'production';
-module.exports = useSentry ? withSentryConfig(baseConfig) : baseConfig;
+module.exports = baseConfig;
