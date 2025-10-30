fdaf1f84cd16b4f74970dfb0c10cf737bdc9e942
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2025-10-31 08:42:05 +0900

    Xcode Cloud向けスクリプト修正 (#4537)
    * brewでnodeを入れる
    
    * brew link

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-\. "$HOME/.nvm/nvm.sh"
-nvm install 24
+brew install node@24
+brew link node@24 --force --overwrite
 npm install -g pnpm@10
 pnpm approve-builds
 pnpm i
