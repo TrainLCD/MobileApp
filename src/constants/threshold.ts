1c0235e7790651a7b35e9b9eceb10acadfcdb753
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-01-25 04:00:12 +0900

    停車判定から減速判定を廃止 (#5132)

--- src/constants/threshold.ts
+++ src/constants/threshold.ts
@@ -1,7 +1,6 @@
 // 閾値
 export const APPROACHING_MAX_THRESHOLD = 1000;
 export const ARRIVED_MAX_THRESHOLD = 500;
-export const ARRIVED_MAXIMUM_SPEED = 35; // km/h
 export const MANY_LINES_THRESHOLD = 7;
 export const OMIT_JR_THRESHOLD = 3; // これ以上JR線があったら「JR線」で省略しよう
 export const JR_LINE_MAX_ID = 6;
