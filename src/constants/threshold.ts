04c06929429087fe32a10bb2f16fb656e720cba2
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-01-26 19:23:29 +0900

    到着閾値を500mから300mに縮小 (#5170)

--- src/constants/threshold.ts
+++ src/constants/threshold.ts
@@ -1,6 +1,6 @@
 // 閾値
 export const APPROACHING_MAX_THRESHOLD = 1000;
-export const ARRIVED_MAX_THRESHOLD = 500;
+export const ARRIVED_MAX_THRESHOLD = 300;
 export const MANY_LINES_THRESHOLD = 7;
 export const OMIT_JR_THRESHOLD = 3; // これ以上JR線があったら「JR線」で省略しよう
 export const JR_LINE_MAX_ID = 6;
