1ea56d38d2a85bef44298d87a7c645cac6073571
TinyKitten <32848922+TinyKitten@users.noreply.github.com>
2026-01-28 00:59:21 +0900

    Bump version for canary release

--- app.config.ts
+++ app.config.ts
@@ -52,7 +52,7 @@ export default ({ config }: ConfigContext) => ({
         ? 'me.tinykitten.trainlcd'
         : 'me.tinykitten.trainlcd.dev',
     permissions: [],
-    versionCode: 100000213,
+    versionCode: 100000214,
   },
   owner: 'trainlcd',
 });
