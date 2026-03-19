efbc1369c2dc83c61295bcb1c56baf8943cc5e58
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-03-19 18:49:48 +0900

    英語音声をen-US-Standard-Gに合わせる

--- src/constants/tts.ts
+++ src/constants/tts.ts
@@ -1,2 +1,2 @@
 export const DEFAULT_JA_TTS_VOICE_NAME = 'ja-JP-Standard-B';
-export const DEFAULT_EN_TTS_VOICE_NAME = 'en-US-Standard-F';
+export const DEFAULT_EN_TTS_VOICE_NAME = 'en-US-Standard-G';
