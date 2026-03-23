f7dbfcc2c6d95286661382105ec658962c63a4d0
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-03-24 00:33:39 +0900

    bump to v10.3.0 (#5656)

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import type { ConfigContext } from 'expo/config';
 export default ({ config }: ConfigContext) => ({
   name: 'TrainLCD',
   slug: 'trainlcd',
-  version: '10.2.1',
+  version: '10.3.0',
   plugins: [
     'expo-image',
     'expo-font',
@@ -53,7 +53,7 @@ export default ({ config }: ConfigContext) => ({
     },
   },
   ios: {
-    buildNumber: '2551',
+    buildNumber: '2552',
     bundleIdentifier:
       process.env.EAS_BUILD_PROFILE === 'production'
         ? 'me.tinykitten.trainlcd'
@@ -70,10 +70,11 @@ export default ({ config }: ConfigContext) => ({
         ? 'me.tinykitten.trainlcd'
         : 'me.tinykitten.trainlcd.dev',
     permissions: [],
-    versionCode: 100000338,
+    versionCode: 100000339,
   },
   owner: 'trainlcd',
 });
+
 
 
 
