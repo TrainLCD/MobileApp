f1c43d7fb9aaa7fb3ffdc393809c7648588ed35d
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-04-02 13:13:27 +0900

    小田急テーマヘッダーデザイン調整

--- src/components/HeaderOdakyu.tsx
+++ src/components/HeaderOdakyu.tsx
@@ -2,20 +2,17 @@ import React from 'react';
 import type { CommonHeaderProps } from './Header.types';
 import type { HeaderEastThemeConfig } from './HeaderEast';
 import { HeaderEast, tokyoMetroConfig } from './HeaderEast';
+import isTablet from '~/utils/isTablet';
 
 const odakyuConfig: HeaderEastThemeConfig = {
   ...tokyoMetroConfig,
-  gradientColors: [
-    '#b0b0b0',
-    '#a0a0a0',
-    '#808080',
-    '#b8b8b8',
-    '#c8c8c8',
-  ] as const,
+  gradientColors: ['#fcfcfc', '#a0a0a0', '#808080', '#dedede'] as const,
+  gradientLocations: [0, 0.2, 0.21, 1] as const,
   textColor: '#1a1a1a',
   rootStyle: {},
   divider: {
     ...tokyoMetroConfig.divider,
+    height: isTablet ? 8 : 6,
     color: 'dynamic',
   },
   secondaryDivider: {
