4c321927f8343e75a12878e7353e30e2a7229596
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-06-20 20:02:06 +0900

    v10.8.0 をリリース

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ const IS_DEV = process.env.APP_VARIANT === 'dev';
 export default {
   name: 'TrainLCD',
   slug: 'trainlcd',
-  version: '10.7.1',
+  version: '10.8.0',
   plugins: [
     'expo-image',
     'expo-font',
@@ -52,7 +52,7 @@ export default {
     },
   },
   ios: {
-    buildNumber: '2715',
+    buildNumber: '2716',
     scheme: IS_DEV ? 'CanaryTrainLCD' : 'ProdTrainLCD',
     bundleIdentifier: IS_DEV ? 'me.tinykitten.trainlcd.dev' : 'me.tinykitten.trainlcd',
     supportsTablet: true,
@@ -60,7 +60,7 @@ export default {
   android: {
     package: IS_DEV ? 'me.tinykitten.trainlcd.dev' : 'me.tinykitten.trainlcd',
     permissions: [],
-    versionCode: 100000502,
+    versionCode: 100000503,
   },
   owner: 'trainlcd',
   experiments: {
