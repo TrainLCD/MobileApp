41ab6a632b18e560f0f75f0ac4236781e85f54aa
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-06-20 17:53:41 +0900

    Add configuration for Xcode Cloud

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+echo "$SENTRY_PROPERTIES_BASE64" | base64 --decode > /Volumes/workspace/repository/ios/sentry.properties
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to write sentry.properties file"
+  exit 1
+fi
+
+brew install node@24
+brew link node@24 --force --overwrite
+
+npm ci
+pod install --repo-update
