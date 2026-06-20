f588f82bf92a40c467185dfdead0107f267695f8
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-06-20 17:37:36 +0900

    Add: ios.bundleIdentifier

--- app.config.ts
+++ app.config.ts
@@ -54,6 +54,7 @@ export default {
   ios: {
     buildNumber: '2715',
     scheme: IS_DEV ? 'CanaryTrainLCD' : 'ProdTrainLCD',
+    bundleIdentifier: IS_DEV ? 'me.tinykitten.trainlcd.dev' : 'me.tinykitten.trainlcd',
     supportsTablet: true,
   },
   android: {
