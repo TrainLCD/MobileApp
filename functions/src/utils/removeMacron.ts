cd75e425b87991895036f42226c6f09601b6f254
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-06-23 10:05:46 +0900

    BFFリポジトリへ移行済みのfunctionsディレクトリと関連参照を削除 (#6306)

--- functions/src/utils/removeMacron.ts
+++
@@ -1,9 +0,0 @@
-export const removeMacron = (str: string): string => {
-  return str
-    .replace(/[ŌŪ]/g, (match) => {
-      return match.replace('Ō', 'O').replace('Ū', 'U');
-    })
-    .replace(/[ōū]/g, (match) => {
-      return match.replace('ō', 'o').replace('ū', 'u');
-    });
-};
