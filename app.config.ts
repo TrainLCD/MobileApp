b3d9e62bc6648e0e770176ba5b4dd316e86a60d5
TinyKitten <32848922+TinyKitten@users.noreply.github.com>
2026-07-23 09:21:53 +0900

    Bump version for canary release

--- app.config.ts
+++ app.config.ts
@@ -52,7 +52,7 @@ export default {
     },
   },
   ios: {
-    buildNumber: '2754',
+    buildNumber: '2755',
     scheme: IS_DEV ? 'CanaryTrainLCD' : 'ProdTrainLCD',
     bundleIdentifier: IS_DEV ? 'me.tinykitten.trainlcd.dev' : 'me.tinykitten.trainlcd',
     supportsTablet: true,
@@ -60,7 +60,7 @@ export default {
   android: {
     package: IS_DEV ? 'me.tinykitten.trainlcd.dev' : 'me.tinykitten.trainlcd',
     permissions: [],
-    versionCode: 100000541,
+    versionCode: 100000542,
   },
   owner: 'trainlcd',
   experiments: {
