93df2830297fe8add4860b297e2034a94f6c0d29
TinyKitten <32848922+TinyKitten@users.noreply.github.com>
2026-08-04 08:29:32 +0900

    Bump version to 10.11.1

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ const IS_DEV = process.env.APP_VARIANT === 'dev';
 export default {
   name: 'TrainLCD',
   slug: 'trainlcd',
-  version: '10.11.0',
+  version: '10.11.1',
   plugins: [
     'expo-image',
     'expo-font',
@@ -52,7 +52,7 @@ export default {
     },
   },
   ios: {
-    buildNumber: '2794',
+    buildNumber: '2795',
     scheme: IS_DEV ? 'CanaryTrainLCD' : 'ProdTrainLCD',
     bundleIdentifier: IS_DEV ? 'me.tinykitten.trainlcd.dev' : 'me.tinykitten.trainlcd',
     supportsTablet: true,
@@ -60,7 +60,7 @@ export default {
   android: {
     package: IS_DEV ? 'me.tinykitten.trainlcd.dev' : 'me.tinykitten.trainlcd',
     permissions: [],
-    versionCode: 100000557,
+    versionCode: 100000558,
   },
   owner: 'trainlcd',
   experiments: {
