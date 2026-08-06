9db826202953cebd37ac49bbac459e8347e3fcb8
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-08-06 10:05:36 +0900

    ダイアログのアイコンとタイトルの間隔を縮小

--- src/components/DialogModalLayout.tsx
+++ src/components/DialogModalLayout.tsx
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     width: 48,
     height: 48,
     overflow: 'hidden',
-    marginRight: 12,
+    marginRight: 4,
     justifyContent: 'center',
     alignItems: 'center',
   },
