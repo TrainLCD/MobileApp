c34b0891083a6c00add4ec14b7ec2821e97e10df
TinyKitten <32848922+TinyKitten@users.noreply.github.com>
2026-08-12 19:37:19 +0900

    Bump version for canary release

--- app.config.ts
+++ app.config.ts
@@ -54,7 +54,7 @@ export default {
   ios: {
     // Expo SDK 57 の各モジュール（expo / expo-modules-core ほか）は podspec で iOS 16.4 以上を要求する
     deploymentTarget: '16.4',
-    buildNumber: '2833',
+    buildNumber: '2834',
     scheme: IS_DEV ? 'CanaryTrainLCD' : 'ProdTrainLCD',
     bundleIdentifier: IS_DEV ? 'me.tinykitten.trainlcd.dev' : 'me.tinykitten.trainlcd',
     supportsTablet: true,
@@ -62,7 +62,7 @@ export default {
   android: {
     package: IS_DEV ? 'me.tinykitten.trainlcd.dev' : 'me.tinykitten.trainlcd',
     permissions: [],
-    versionCode: 100000607,
+    versionCode: 100000609,
   },
   owner: 'trainlcd',
   experiments: {
