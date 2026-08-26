03c383136413d8cb4ad3ea75dfc1da96c2df96fb
Tsubasa SEKIGUCHI <oss@tinykitten.me>
2026-08-27 01:31:30 +0900

    Bump version for canary release

--- android/wearable/build.gradle.kts
+++ android/wearable/build.gradle.kts
@@ -85,12 +85,12 @@ android {
       dimension = "environment"
       applicationIdSuffix = ".dev"
       versionNameSuffix = "-dev"
-      versionCode = 100000638
+      versionCode = 100000640
       versionName = "10.13.1"
     }
     create("prod") {
       dimension = "environment"
-      versionCode = 100000638
+      versionCode = 100000640
       versionName = "10.13.1"
     }
   }
