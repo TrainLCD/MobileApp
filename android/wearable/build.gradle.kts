f88179dc3a3062712e9c9c1da71c70d9d198988b
TinyKitten <32848922+TinyKitten@users.noreply.github.com>
2026-08-28 20:02:39 +0900

    Bump version for canary release

--- android/wearable/build.gradle.kts
+++ android/wearable/build.gradle.kts
@@ -85,12 +85,12 @@ android {
       dimension = "environment"
       applicationIdSuffix = ".dev"
       versionNameSuffix = "-dev"
-      versionCode = 100000662
+      versionCode = 100000664
       versionName = "10.13.1"
     }
     create("prod") {
       dimension = "environment"
-      versionCode = 100000662
+      versionCode = 100000664
       versionName = "10.13.1"
     }
   }
